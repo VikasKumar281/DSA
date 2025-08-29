f6196e0eb9cd8e7cf97119812657e45b23d7e479
Vikas Kumar <vikaskumar280204@gmail.com>
2025-08-29 20:33:21 +0530

    Create Find Length of Linked List.cpp

---
+++ Linked List/Find Length of Linked List.cpp
@@ -0,0 +1,27 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getCount(Node* head) {
+        int count =0;
+        Node * temp = head;
+        while(temp!=NULL){
+            temp = temp->next;
+            count++;
+        }
+        
+        return count;
+        
+    }
+};
