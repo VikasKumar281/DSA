59f487ad67dde6ad80fabcc6a1addb64a20ae604
Vikas Kumar <vikaskumar280204@gmail.com>
2025-08-31 15:35:39 +0530

    Create Deletion of head in DLL.cpp

---
+++ Linked List/Doubly Linked List/Deletion of head in DLL.cpp
@@ -0,0 +1,37 @@
+// User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+
+};
+*/
+Node *deleteHead(Node *head) {
+    
+    if(head == NULL){
+        return NULL;
+    } 
+    
+    if(head->next == NULL){
+        delete head;
+        return NULL;
+    }
+    
+    Node* temp = head;
+    head = head->next;
+    head->prev = NULL;
+    
+    delete temp;
+    
+    return head;
+    
+}
