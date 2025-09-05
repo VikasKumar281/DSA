afc8b8f9f9e626e28ed9fe8bad0374861643d278
Vikas Kumar <vikaskumar280204@gmail.com>
2025-09-05 13:01:19 +0530

    Update and rename Reverse Linked List( Iterative ).cpp to Reverse Linked List( Recursive ).cpp

--- Linked List/Reverse Linked List( Recursive ).cpp
+++ Linked List/Reverse Linked List( Recursive ).cpp
@@ -26,3 +26,29 @@ public:
         return newHead;
     }
 };
+
+
+// ITERATIVE APPROACH ------------------------------------------------>
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        if(head == NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+
+        while(curr){
+           next = curr->next;
+           curr->next = prev;
+           prev = curr;
+           curr = next;
+        }
+
+        return prev;
+    }
+};
