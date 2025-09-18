c858efc6bfdefeb431154d53ce4fdbbed5351497
Vikas Kumar <vikaskumar280204@gmail.com>
2025-09-18 10:30:31 +0530

    Create Rearrange a linked list.cpp

---
+++ Linked List/Rearrange a linked list.cpp
@@ -0,0 +1,33 @@
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void rearrangeEvenOdd(Node *head) {
+       
+        Node* odd = head;
+        Node* even = head->next;
+        Node* evenhead = even;
+        
+        while(even && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            
+            even->next = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = evenhead;
+    }
+};
