9b45e78411089e54c8b207254243decb6af6343c
Vikas Kumar <vikaskumar280204@gmail.com>
2025-10-15 16:45:37 +0530

    Create Sort Array By Parity II.cpp

---
+++ Arrays/Sort Array By Parity II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        int even = 0;
+        int odd = 1;
+
+        while(even < n && odd < n){
+            if( nums[even] % 2==0 ){
+                even += 2;
+            }
+            else if(nums[odd] %2 !=0){
+               odd += 2;
+            }
+            else{
+                swap(nums[even] , nums[odd]);
+                even += 2;
+                odd += 2;
+            }
+        }
+
+        return nums;
+    }
+};
+
+// Time complexity: O(n)
+// Space complexity: O(1)
