5a32e1ae414802543ba002e22e338c7981953966
Vikas Kumar <vikaskumar280204@gmail.com>
2025-10-22 10:52:48 +0530

    Improve findKDistantIndices implementation
    Refactor findKDistantIndices function for efficiency.

--- Arrays/Find All K-Distant Indices in an Array.cpp
+++ Arrays/Find All K-Distant Indices in an Array.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int>result;
+        int n = nums.size();
+
+        for(int i =0;i<n;i++){
+         for(int j =0;j<n;j++){
+           if(nums[j] == key && abs(i-j) <= k){
+                result.push_back(i);
+                break;
+           }
+        }
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         vector<int> res;
         int r = 0;  
         int n = nums.size();
