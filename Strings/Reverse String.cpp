ee7f267b16ec0d4cd461bbd1f1506a6f2c081466
Vikas Kumar <vikaskumar280204@gmail.com>
2025-10-22 11:12:14 +0530

    Update Reverse String.cpp

--- Strings/Reverse String.cpp
+++ Strings/Reverse String.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int n = s.size();
+        int i =0 , j = n - 1;
+
+        while(i <= j){
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+ 
+    }
+};
+
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
         vector<char> st(n);
 
         for(int i = 0;i<n;i++){
