eef6fd3b2751c34f3719b9d240888b21352582f0
Vikas Kumar <vikaskumar280204@gmail.com>
2025-10-27 15:04:40 +0530

    Update Shortest Distance to a Character.cpp

--- Strings/Shortest Distance to a Character.cpp
+++ Strings/Shortest Distance to a Character.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         int n = s.size();
-        vector<int> ans(n, n);  
-        int prev = -n; 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == c) prev = i;
-            ans[i] = i - prev;
-        }
-        prev = 2 * n; 
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == c) prev = i;
-            ans[i] = min(ans[i], abs(i - prev));
+        vector<int> temp;
+        
+        for(int i =0;i<n;i++){
+            if(s[i] == c){
+                temp.push_back(i);
+            }
         }
         
+        vector<int>ans(n,0);
+        for(int i =0;i<n;i++){
+            int mini = INT_MAX;
+            for(int pos: temp){
+              mini = min(mini,abs(i-pos));
+            }
+            ans[i] = mini;
+        }
+
         return ans;
     }
 };
+
+
