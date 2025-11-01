6051b23533fcca679f454fca4a7caa9e2d277aef
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-01 12:20:57 +0530

    Create Count Good Numbers.cpp

---
+++ Recursion/Count Good Numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    const long long MOD = 1e9 + 7;
+
+  
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+        while (exp > 0) {
+            if (exp % 2)
+                result = (result * base) % MOD;
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long evenCount = (n + 1) / 2;
+        long long oddCount = n / 2;        
+
+        long long ans = (modPow(5, evenCount) * modPow(4, oddCount)) % MOD;
+        return (int)ans;
+    }
+};
