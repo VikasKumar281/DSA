ce59272e5da1fec6b439f363a7f53adf1bb2512b
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-11 14:06:22 +0530

    Create Maximum Product Subarray.cpp

---
+++ Arrays/Maximum Product Subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int result = nums[0];
+        int product = 1;
+
+        for(int i =0;i<n;i++){
+            product = product * nums[i];
+            result = max(product , result);
+            if(product == 0){
+              product = 1;
+            }
+        }
+          
+        product = 1;
+        for(int i = n-1;i>=0;i--){
+            product = product * nums[i];
+            result = max(product , result);
+
+            if(product == 0){
+             product = 1;
+            }
+        }
+
+        return result;
+    }
+};
