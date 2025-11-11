4597055fd4a73375e2b6c2f05c05bf194125e749
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-11 16:37:34 +0530

    Create Rotate String.cpp

---
+++ Strings/Rotate String.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n  = s.size();
+        int m = goal.size();
+
+        if(n != m){
+            return false;
+        }
+        
+        queue<char> q1,q2;
+
+        for(int i=0;i<n;i++){
+            q1.push(s[i]);
+        }
+        for(int i=0;i<m;i++){
+            q2.push(goal[i]);
+        }
+        
+        for(int i = 0;i<n;i++){
+            char ch = q1.front();
+            q1.pop();
+            q1.push(ch);
+            if(q1 == q2){
+               return true;
+            }
+        }
+
+        return false;
+    }
+};
