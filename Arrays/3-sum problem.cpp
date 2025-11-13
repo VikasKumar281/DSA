55cfbd419ec49a0c1c0f4aed655c4a9b08780cb3
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-13 12:42:01 +0530

    Update 3-sum problem.cpp

--- Arrays/3-sum problem.cpp
+++ Arrays/3-sum problem.cpp
@@ -1,38 +1,82 @@
-class Solution {
+// Optimal Approach ---------------------------------------------------------------------------------->
+class Solution {   
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-
-                    // Skip duplicates for left and right
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                }
-                else if (sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i =0;i<n;i++){
+          if(i>0 && nums[i] == nums[i-1]){
+            continue;
+          }
+          int j = i+1;
+          int k = n-1;
+          while(j<k){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0){
+               j++;
             }
+            else if(sum > 0){
+               k--;
+            }
+            else{
+               vector<int> temp = {nums[i] , nums[j] , nums[k]};
+               ans.push_back(temp);
+               j++;
+               k--;
+
+               while(j < k && nums[j] == nums[j-1]){
+                j++;
+               }
+               while(j < k && nums[k] == nums[k+1]){
+                k--;
+               }
+            }
+          }
+        }
+        return ans;  
+    }
+};
+
+// Time complexity =>	O(n^2)
+// Space	O(1)
+
+
+
+
+// Brute Force Approach --------------------------------------------------------------->
+class Solution {
+private: 
+    void sorting(vector<int> &arr){
+        int n = arr.size();
+        for(int i =0;i<n-1;i++){
+          int mini = i;
+          for(int j = i+1;j<n;j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+          }
+         swap(arr[i],arr[mini]);
+        }
+    }    
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> st;
+        for(int i =0;i<n;i++){
+           for(int j = i+1;j<n;j++){
+             for(int k = j+1;k<n;k++){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    vector<int>temp = {nums[i] , nums[j] , nums[k]};
+                    sorting(temp);
+                    st.insert(temp);
+                }
+             }
+           }
         }
 
-        return result;
+        vector<vector<int>> ans(st.begin() , st.end());
+
+        return ans;  
     }
 };
