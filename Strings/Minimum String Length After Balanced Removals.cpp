626286e480773f059368ce9ceaf1ab89cf06532a
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-18 18:05:53 +0530

    Implement minLengthAfterRemovals function

---
+++ Strings/Minimum String Length After Balanced Removals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int n = s.size();
+
+        int count1 =0;
+        int count2 =0;
+
+        for(int i =0;i<n;i++){
+            if(s[i] == 'a'){
+                count1++;
+            }
+            else{
+                count2++;
+            }
+        }
+
+       return abs(count2-count1);
+         
+    }
+};
