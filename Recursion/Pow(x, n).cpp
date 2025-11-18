af76063c231f59ef44aafe1d89679ecde2568b0d
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-18 16:04:27 +0530

    Update Pow(x, n).cpp

--- Recursion/Pow(x, n).cpp
+++ Recursion/Pow(x, n).cpp
@@ -1,4 +1,36 @@
 class Solution {
+private: 
+    double fastPow(double x , int n){
+        if(n == 0){
+            return 1.0;
+        }
+
+        double half = fastPow(x , n/2);
+
+        if(n % 2 == 0){
+            return half * half;
+        }
+
+      return half * half * x;  
+
+    }    
+public:
+    double myPow(double x, int n) { 
+        long long N = n;
+
+        if(N < 0){
+           return 1.0 / fastPow(x, -N);
+        } 
+
+        return fastPow(x,N);
+
+    }
+};
+
+
+
+
+class Solution {
 public:
     double myPow(double x, int n) {
         long long N = n;
