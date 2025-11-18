6406779d94b961bab7157f43caa792844b79caf6
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-18 18:26:05 +0530

    Optimize character counting in minLengthAfterRemovals
    Refactor minLengthAfterRemovals to use unordered_map for counting characters.

--- Strings/Minimum String Length After Balanced Removals.cpp
+++ Strings/Minimum String Length After Balanced Removals.cpp
@@ -3,6 +3,25 @@ public:
     int minLengthAfterRemovals(string s) {
         int n = s.size();
 
+        unordered_map<char,int>mp;
+        
+        for(char ch:s){
+            mp[ch]++;
+        }
+
+        int count1 = mp['a'];
+        int count2 = mp['b'];
+
+        return abs(count1 - count2); 
+    }
+};
+
+
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int n = s.size();
+
         int count1 =0;
         int count2 =0;
 
