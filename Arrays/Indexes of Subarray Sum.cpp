dc89a10dfb0f67769025aa721025ad0d04b4e4be
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-21 11:10:09 +0530

    Add subarray sum solution in C++
    Implement a solution to find the starting and ending indices of a subarray that sums to a given target.

---
+++ Arrays/Indexes of Subarray Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        int n = arr.size();
+        int start =0;
+        long long sum = 0;
+
+        for(int end =0;end<n;end++){
+            sum = sum + arr[end];
+            while(sum > target && start <=end){
+               sum -= arr[start];
+               start++;
+            }
+            
+            if(sum == target){
+                return{start+1,end+1};
+            }
+        }
+        
+        return {-1};
+    }
+};
