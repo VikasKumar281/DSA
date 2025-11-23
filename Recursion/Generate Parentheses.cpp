753d1c181362e71cdb431c19857ee5cefc8de4ef
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-23 11:35:05 +0530

    Update Generate Parentheses.cpp

--- Recursion/Generate Parentheses.cpp
+++ Recursion/Generate Parentheses.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    void solve(vector<string> &ans ,string &curr, int n,int open , int close){
+        if(curr.length() == 2*n){
+              ans.push_back(curr);
+            return;
+        }
+        
+        if(open < n){
+           curr.push_back('(');
+           solve(ans,curr,n,open+1,close);
+           curr.pop_back();
+        }
+
+        if(close < open){
+           curr.push_back(')');
+           solve(ans,curr,n,open,close+1);
+           curr.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string curr = "";
+        solve(ans,curr,n,0,0);
+
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
     bool isValid(string &str){
         int count = 0;
         for(char c:str){
