9c5d4ae0ba69c84b380bd46809b18ad2c7afc162
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-28 22:03:48 +0530

    Create Majority Element.cpp

---
+++ Arrays/Majority Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int candidate = -1, count = 0;
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) count++;
+        }
+
+        return (count > arr.size() / 2) ? candidate : -1;
+    }
+};
