bc7b3682a8697b4acef3c5693368652d842d2360
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-29 12:59:21 +0530

    Update Majority Element.cpp

--- Arrays/Majority Element.cpp
+++ Arrays/Majority Element.cpp
@@ -1,4 +1,31 @@
 class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+       int n = arr.size();
+       map<int,int> freq;
+       
+       for(int i =0;i<n;i++){
+           freq[arr[i]]++;
+       }
+       int maxiIndex = 0;
+       for(int i =1;i<n;i++){
+           if(freq[arr[i]] > freq[arr[maxiIndex]]){
+               maxiIndex = i;
+           }
+       }
+       
+       if(freq[arr[maxiIndex]] > n/2){
+           return arr[maxiIndex];
+       }
+       else
+         return -1;  
+    }
+};   
+
+
+
+
+class Solution {
 public:
     int majorityElement(vector<int>& arr) {
         int candidate = -1, count = 0;
@@ -14,7 +41,8 @@ public:
         }
         count = 0;
         for (int num : arr) {
-            if (num == candidate) count++;
+            if (num == candidate)
+                count++;
         }
 
         return (count > arr.size() / 2) ? candidate : -1;
