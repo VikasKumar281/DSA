30a42ae15d9a2b5f868810126e8a792e55afdbde
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-30 17:10:29 +0530

    Update Frog Jump.cpp

--- DP/1D DP/Frog Jump.cpp
+++ DP/1D DP/Frog Jump.cpp
@@ -1,5 +1,30 @@
 class Solution {
   public:
+    int minCost(vector<int>& height) {
+       int n = height.size();
+       int prev2 = 0;
+       int prev = 0;
+       
+       for(int i =1;i<n;i++){
+           int first = prev + abs(height[i] - height[i-1]);
+           int second = INT_MAX;
+           if(i>1){
+               second = prev2 + abs(height[i] - height[i-2]);
+           }
+           
+           int curr = min(first , second);
+           prev2 = prev;
+           prev = curr;
+       }
+       
+       return prev;
+    }
+};
+
+
+// MEMOISATION -------------------------------------------------------------------------->
+class Solution {
+  public:
     int func(int index , vector<int> &dp , vector<int> &height){
         if(index == 0){
             return 0;
@@ -20,7 +45,7 @@ class Solution {
     }
     int minCost(vector<int>& height) {
        int n = height.size();
-       vector<int> dp(n,-1);
+       vector<int> dp(n+1,-1);
        return func(n-1,dp,height);
     }
 };
