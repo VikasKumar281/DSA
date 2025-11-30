9ab9c76ca0aa964c85d8ce0af66bf901277a2caf
Vikas Kumar <vikaskumar280204@gmail.com>
2025-11-30 18:17:22 +0530

    Create Maximum sum of non-adjacent elements(House Robber).cpp

---
+++ DP/1D DP/Maximum sum of non-adjacent elements(House Robber).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(int index , vector<int> &dp , vector<int> &nums){
+        if(index < 0){
+            return 0;
+        }
+        if(index == 0){
+            return nums[0];
+        }
+
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int pick = func(index-2,dp,nums) + nums[index];
+        
+        int skip = func(index-1,dp,nums) + 0;
+
+        return max(pick,skip); 
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return func(n-1,dp,nums);
+    }
+};
