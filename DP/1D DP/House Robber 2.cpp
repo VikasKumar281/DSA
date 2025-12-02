fd43f0b2588ae6fe422efe0fc4ff2a3f40b178bd
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-02 20:54:29 +0530

    Implement House Robber II solution in C++

---
+++ DP/1D DP/House Robber 2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int func(int index, vector<int> &nums, vector<int> &dp) {
+        if(index < 0) {
+            return 0;  
+        }
+
+        if(index == 0) {
+            return nums[0]; 
+        }
+
+        if(dp[index] != -1) {
+            return dp[index];
+        }
+
+        int pick = nums[index] + func(index - 2, nums, dp);
+
+        int skip = func(index - 1, nums, dp);
+
+        return dp[index] = max(pick, skip);
+    }
+
+    int robLinear(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return func(n - 1, nums, dp);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) {
+            return nums[0];
+        }
+        vector<int> a(nums.begin(), nums.end() - 1);
+
+        vector<int> b(nums.begin() + 1, nums.end());
+
+        return max(robLinear(a), robLinear(b));
+    }
+};
