3f54ef60aafe4c998df62d8f9b6a90418c41d976
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-03 13:44:27 +0530

    Add House Robber II solution with circular handling
    Implemented a solution for the House Robber II problem, handling both circular and linear cases.

--- DP/1D DP/House Robber 2.cpp
+++ DP/1D DP/House Robber 2.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    int robLinear(vector<int> &nums){
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        if(n==0){
+            return 0;
+        }
+        vector<int> dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+
+        for(int i =2;i<n;i++){
+            int pick = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+
+            dp[i] = max(pick,skip);
+        }
+        return dp[n-1];
+    }
+    // For Circular -------------------------------------->
+    int rob(vector<int>& nums) {
+      int n = nums.size()-1;
+      if(n == 0){
+        return nums[0];
+      }
+      vector<int> case1(nums.begin(),nums.end() - 1);         
+      vector<int> case2(nums.begin() + 1,nums.end());
+
+      return max(robLinear(case1) , robLinear(case2));         
+    }
+};
+
+
+
+
+class Solution {
+public:
     int func(int index, vector<int> &nums, vector<int> &dp) {
         if(index < 0) {
             return 0;  
