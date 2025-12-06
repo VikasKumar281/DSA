4b3567fa8eb43b5d20ee8a0c6d2badd4754121d8
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-06 19:07:52 +0530

    Update Combination Sum.cpp

--- Recursion/Combination Sum.cpp
+++ Recursion/Combination Sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        makeCombination(candidates, target, 0, comb, 0, res);
-        return res;        
+    void findCombinationSum(int index,int target ,vector<int>& candidates,vector<vector<int>>& ans,vector<int>& ds){
+        if(index == candidates.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        // Pick the element ------------>
+        if(candidates[index] <= target){
+          ds.push_back(candidates[index]);
+          findCombinationSum(index,target-candidates[index],candidates,ans,ds);
+          ds.pop_back();
+        }
+
+        // Skip the element ------------>
+        findCombinationSum(index+1,target,candidates,ans,ds);
+
     }
-
-private:
-    void makeCombination(std::vector<int>& candidates, int target, int idx, vector<int>& comb, int total, vector<vector<int>>& res) {
-        if (total == target) {
-            res.push_back(comb);
-            return;
-        }
-
-        if (total > target || idx >= candidates.size()) {
-            return;
-        }
-
-        comb.push_back(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.pop_back();
-        makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>ds;
+        vector<vector<int>> ans;
+        findCombinationSum(0,target,candidates,ans,ds);
+        return ans;
+    }
 };
+
