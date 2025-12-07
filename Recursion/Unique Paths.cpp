5a05eee7e4834de3c624fbdc2c589af8b3a06d15
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-07 20:39:19 +0530

    Create Unique Paths.cpp

---
+++ Recursion/Unique Paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int func(int i , int j){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        
+        int up = func(i-1,j);
+        int left = func(i,j-1);
+
+        return up + left;
+    }
+    int uniquePaths(int m, int n) {
+        return func(m-1,n-1);
+    }
+};
