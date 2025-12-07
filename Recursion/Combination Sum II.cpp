d13ecd238817d88935a9ff26e2e2a2e56dc7c46c
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-07 17:45:00 +0530

    Implement Combination Sum II algorithm

---
+++ Recursion/Combination Sum II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void helper(int index, int target,vector<vector<int>> &ans,vector<int> &arr,vector<int> &ds){
+        if(target == 0){
+          ans.push_back(ds);
+          return;
+        }
+
+       for(int i = index; i < arr.size(); i++) {
+            if(i > index && arr[i] == arr[i-1]){ 
+                continue;
+            }
+            if(arr[i] > target) {
+                break;
+            }
+            ds.push_back(arr[i]);
+            helper(i + 1, target - arr[i], ans, arr, ds);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int>ds;
+        vector<vector<int>> ans;
+        helper(0,target,ans,candidates,ds);
+        return ans;
+    }
+};
