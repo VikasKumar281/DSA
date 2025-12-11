f316a27ea0b1de833a048d83c99dc4229e403bc9
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-11 17:18:09 +0530

    Add memoization approach for unique paths with obstacles

--- DP/2D DP/Unique Paths II.cpp
+++ DP/2D DP/Unique Paths II.cpp
@@ -1,3 +1,4 @@
+// TABULATION APPROACH ----------------------------------------------------------------------------------------------------------->
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
@@ -28,5 +29,39 @@ public:
         }
 
         return dp[m-1][n-1];  
+    }
+};
+
+
+
+// MEMOIZATION APPROACH ----------------------------------------------------------------------------------------------------------->
+class Solution {
+public:
+    int helper(vector<vector<int>>& obstacleGrid,int i , int j, vector<vector<int>> &dp){
+        if(i>=0 && j>=0 && obstacleGrid[i][j] != 0){
+            return 0;
+        }
+
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i <0 || j<0){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int up = helper(obstacleGrid,i-1,j,dp);
+        int left = helper(obstacleGrid,i,j-1,dp);
+
+        return dp[i][j] = up + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return helper(obstacleGrid,m-1,n-1,dp);
     }
 };
