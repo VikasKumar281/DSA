34992e84b0e18be7768c1dfcb3ff33dfc471ddff
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-13 15:49:49 +0530

    Update Number of Substrings Containing All Three Characters.cpp

--- Strings/Number of Substrings Containing All Three Characters.cpp
+++ Strings/Number of Substrings Containing All Three Characters.cpp
@@ -3,6 +3,28 @@ public:
     int numberOfSubstrings(string s) {
         int n = s.size();
         int count =0;
+        vector<int> lastSeen(3,-1);
+
+        for(int i =0;i<n;i++){
+            lastSeen[s[i] - 'a'] = i;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[0] != -1){
+              count= count + (1 + min(lastSeen[0] , min(lastSeen[1] , lastSeen[2])));
+            }
+        } 
+
+        return count;
+    }
+};
+// T.C = O(N)
+// S.C = O(1)
+
+
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int count =0;
         for(int i =0;i<n;i++){
             vector<int> hash(3,0);
             for(int j =i;j<n;j++){
