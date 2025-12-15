d7b9203b12c045474113ed82ffc2a451c172d13d
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-15 18:54:03 +0530

    Update Minimum Falling Path Sum.cpp

--- DP/2D DP/Minimum Falling Path Sum.cpp
+++ DP/2D DP/Minimum Falling Path Sum.cpp
@@ -1,3 +1,51 @@
+// TABULATION APPROACH -------------------------------------------------------------------------------->
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+
+        //BASE CASE ------>
+        for(int j = 0;j<n;j++){
+            dp[0][j] = matrix[0][j];
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j =0;j<n;j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int leftdiag = matrix[i][j];
+                if(j-1 >= 0){
+                  leftdiag += dp[i-1][j-1];
+                }
+                else{
+                    leftdiag += 1e8;
+                }
+
+                int rightdiag = matrix[i][j];
+                if(j+1 < m){
+                  rightdiag += dp[i-1][j+1];
+                }
+                else{
+                    rightdiag += 1e8;
+                }
+
+                dp[i][j] = min(up , min(leftdiag , rightdiag));
+            }
+        }
+
+        int mini = 1e9;
+        for(int i =0;i<n;i++){
+            mini = min(mini , dp[m-1][i]);
+        }
+
+        return mini;
+    }
+};
+//T.C = O(N * M)  
+//S.C = O(N * M)
+
+
 // MEMOIZATION APPROACH -------------------------------------------------------------------------------->
 class Solution {
 public:
