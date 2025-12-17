89268d555a2fe3d390824ddd60334d465748a476
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-17 17:47:38 +0530

    Update Rearrange Array Elements by Sign.cpp

--- Arrays/Rearrange Array Elements by Sign.cpp
+++ Arrays/Rearrange Array Elements by Sign.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>pos;
+        vector<int>neg;
+        int n = nums.size();
+        vector<int> ans;
+
+        for(int i =0;i<n;i++){
+            if(nums[i] >= 0){
+                pos.push_back(nums[i]);
+            }
+            else{
+                neg.push_back(nums[i]);
+            }
+        }
+         
+        int i =0 , j=0; 
+        int n1 = pos.size() , n2 = neg.size();
+
+        while(i<n1 && j<n2){
+            ans.push_back(pos[i]);
+            i++;
+            ans.push_back(neg[j]);
+            j++;
+        }
+        return ans;
+    }
+};
+//T.C. = O(N)
+//S.C. = O(N)
+
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> pos, neg;
 
         for (int num : nums) {
