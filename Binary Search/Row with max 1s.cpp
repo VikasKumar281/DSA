0aa24c28785bc968bf882d0c5e28ac5b4c0bfa1c
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-19 12:57:57 +0530

    Update Row with max 1s.cpp

--- Binary Search/Row with max 1s.cpp
+++ Binary Search/Row with max 1s.cpp
@@ -4,6 +4,30 @@ class Solution {
         int m = arr.size();
         int n = arr[0].size();
         
+        int row = -1;
+        int col = n-1;
+        
+        for(int i = 0;i<m;i++){
+           while(col >= 0 && arr[i][col] == 1){
+               row = i;
+               col--;
+           }
+        }
+        
+        return row;
+    }
+};
+//T.C. = O(m + n)
+//S.C. = O(1)
+
+
+
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        int m = arr.size();
+        int n = arr[0].size();
+        
         int maxi = 0;
         int count =0;
         int row = -1;
