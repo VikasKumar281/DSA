98882f517770bee353816d45aafab23a38f9fb6d
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-19 12:25:20 +0530

    Update Search a 2D Matrix II.cpp

--- Binary Search/Search a 2D Matrix II.cpp
+++ Binary Search/Search a 2D Matrix II.cpp
@@ -4,6 +4,33 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
+        int rowIndex = 0 , colIndex = n-1;
+
+        while(rowIndex < m && colIndex >= 0){
+            if(matrix[rowIndex][colIndex] == target){
+                return true;
+            }
+            else if(matrix[rowIndex][colIndex] > target){
+                colIndex--;
+            }
+            else{
+                rowIndex++;
+            }
+        }
+
+        return false;
+    }
+};
+//T.C. = O(m + n)
+//S.C. = O(1)
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
         for(int i =0;i<m;i++){
             for(int j =0;j<n;j++){
                 if(matrix[i][j] == target){
