c0ab1cb3e7ca01299db75ddbfbfce253f4916ee0
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-19 19:17:47 +0530

    Update Subset Sum Problem.cpp

--- DP/DP on Subsequences/Subset Sum Problem.cpp
+++ DP/DP on Subsequences/Subset Sum Problem.cpp
@@ -1,3 +1,40 @@
+// TABULATION APPROACH ---------------------------------------------------------------------------------->
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<bool>>dp(n ,vector<bool>(sum+1, false));
+        
+        // BASE CASE --------------------------->
+        for(int index =0;index<n;index++){
+            // target == 0
+            dp[index][0] = true;
+        }
+        
+        if(arr[0] <= sum){
+            // index == 0
+            dp[0][arr[0]] = true;
+        }
+        
+        for(int index = 1;index < n;index++){
+            for(int target = 1;target <= sum;target++){
+                bool notTake = dp[index-1][target];
+                bool take = false;
+                if(arr[index] <= target){
+                 take = dp[index-1][target- arr[index]];
+                }
+                
+                dp[index][target] = take || notTake;
+            }
+        }
+        
+        return dp[n-1][sum];
+    }
+};
+//T.C. = O(n × sum)
+//Space Complexity = O(n × sum)
+
+
 // Memoization Approach ----------------------------------------------------------------------------->
 class Solution {
   public:
