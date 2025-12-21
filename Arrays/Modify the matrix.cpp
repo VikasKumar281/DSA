29e8392e42610be834f4f5d9ed05a715189bbdc3
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-21 22:14:44 +0530

    Create Modify the matrix.cpp

---
+++ Arrays/Modify the matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
+        vector<vector<int>> answer;
+        answer = matrix;
+        int m = answer.size();
+        int n = answer[0].size();
+
+        vector<int> colMax(n,INT_MIN);
+        for(int j = 0; j < n; j++) {
+            for(int i = 0; i < m; i++) {
+                if(matrix[i][j] != -1){
+                    colMax[j] = max(colMax[j], matrix[i][j]);
+                }
+            }
+        }
+
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(answer[i][j] == -1){
+                    answer[i][j] = colMax[j];     
+                }
+            }
+        }
+
+        return answer;
+    }
+};
