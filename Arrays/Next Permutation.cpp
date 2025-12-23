a64231c0c82e2f05a8225dfd3b6d13e6395149b2
Vikas Kumar <vikaskumar280204@gmail.com>
2025-12-23 16:17:25 +0530

    Update Next Permutation.cpp

--- Arrays/Next Permutation.cpp
+++ Arrays/Next Permutation.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+        for(int i = n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+ind+1,nums.end());
+    }
+};
+// Time Complexity = O(n)
+//Space Complexity = O(1)
+
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
       // Using built-in STL library =>
        next_permutation(nums.begin(),nums.end()); 
     }
