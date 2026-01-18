2b12caeb498cf4ee56f446fdaf8820b46941d116
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-18 17:37:02 +0530

    Update Count the number of possible triangles.cpp

--- Arrays/Count the number of possible triangles.cpp
+++ Arrays/Count the number of possible triangles.cpp
@@ -1,3 +1,31 @@
+class Solution {
+  public:
+    int countTriangles(vector<int>& arr) {
+       int n = arr.size();
+       if(n<3){
+           return 0;
+       }
+       int count =0;
+       sort(arr.begin(),arr.end());
+       
+       for (int k=n-1;k>=2; k--) {
+         int i =0 , j = k - 1;
+         while(i<j){
+             if(arr[i] + arr[j] > arr[k]){
+                 count += (j - i);
+                 j--;
+             }
+             else{
+                 i++;
+             }
+         }
+       }
+       return count;
+    }
+};
+//T.C. = O(N^2)
+//S.C. = O(1)
+
 
 // TIME LIMIT EXCEEDED(T.C. = O(N^3)) ----------------------------------------------->
 class Solution {
