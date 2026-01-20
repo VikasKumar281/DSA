026899a4f4db890baf32ae798c940302d19c9e5d
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-20 13:14:56 +0530

    Update Perfect Sum Problem(DP-17).cpp
    Updated the mod variable to include an additional 7 for better handling of large numbers. Enhanced the perfectSum function with a tabulation approach for calculating the number of subsets that sum to the target.

--- DP/DP on Subsequences/Perfect Sum Problem(DP-17).cpp
+++ DP/DP on Subsequences/Perfect Sum Problem(DP-17).cpp
@@ -1,6 +1,46 @@
+// TABULATION APPROACH ------------------------------------------------------------------------>
 class Solution {
   public:
-    int mod = 1e9;
+    int mod = 1e9 + 7;
+    int perfectSum(vector<int>& arr, int target) {
+       int n = arr.size();
+       vector<vector<int>> dp(n , vector<int>(target+1,0));
+       
+       //BASE CASE FOR INDEX 0 --------------------------------->
+       if(arr[0] == 0){
+           dp[0][0]=2;
+       }
+       else{
+           dp[0][0]=1;
+       }
+       
+       if(arr[0] != 0 && arr[0] <= target){
+           dp[0][arr[0]] = 1;
+       }
+       
+       for(int index =1;index < n;index++){
+           for(int sum = 0;sum <= target;sum++){
+               int skip = dp[index-1][sum];
+               int pick = 0;
+               if(arr[index] <= sum){
+                   pick = dp[index-1][sum - arr[index]];
+               }
+               
+               dp[index][sum] = (pick + skip) % mod;
+           }
+       }
+       return dp[n-1][target];
+    }
+};
+//Time: O(n × target)
+// Space: O(n × target)
+
+
+
+//MEMOIZATION APPROACH ------------------------------------------------------------------------->
+class Solution {
+  public:
+    int mod = 1e9+7;
     // We use modulo because the number of subsets can become very large.
     
     int helper(int index,vector<int> &arr,vector<vector<int>> &dp,int target){
