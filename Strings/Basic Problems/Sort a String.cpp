5c2e7e3186e44708cfdcbc11044a5a11b181a9c7
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-22 16:17:00 +0530

    Update Sort a String.cpp

--- Strings/Basic Problems/Sort a String.cpp
+++ Strings/Basic Problems/Sort a String.cpp
@@ -1,4 +1,22 @@
 string sort(string s) {
-    sort(s.begin(),s.end());
+    int n = s.size();
+    
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(s[i]>s[j]){
+                swap(s[i], s[j]);
+            }
+        }
+    }
     return s;
 }
+//T.C. = O(N^2);
+//S.C. = O(1)
+
+
+string sort(string s) {
+    std::sort(s.begin(),s.end());
+    return s;
+}
+//T.C.= O(Nlog N);
+//S.C.= O(log N);
