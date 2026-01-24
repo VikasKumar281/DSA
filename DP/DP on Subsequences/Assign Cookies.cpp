7ad4b25435e4c42bf80133f0a6025cc6ec92c3c0
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-24 14:31:04 +0530

    Create Assign Cookies.cpp
    Added time and space complexity comments to the Assign Cookies function.

---
+++ DP/DP on Subsequences/Assign Cookies.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int m = g.size();
+        int n = s.size();
+
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        
+        int i =0,j=0;
+        int count =0;
+        while(i<m && j<n){
+           if(g[i] <= s[j]){
+            count++;
+            i++;
+            j++;
+           }
+           else{
+            j++;
+           }
+        }
+
+        return count;
+    }
+};
+// Overall Time Complexity:O(mlogm+nlogn)
+//Space Complexity:O(1)
+	​
