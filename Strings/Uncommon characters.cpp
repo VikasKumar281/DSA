8cd3115e42611b3ac3d5a8eab5d7db66397101cf
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-25 15:58:31 +0530

    Create Uncommon characters.cpp

---
+++ Strings/Uncommon characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    string uncommonChars(string& s1, string& s2) {
+        string ans ="";
+        unordered_map<char,int> mp;
+        for(char ch : s2){
+            mp[ch]++;
+        }
+        int i = 0;
+        while(i < s1.size()){
+            if(mp.find(s1[i]) == mp.end()){
+                ans.push_back(s1[i]);
+                mp[s1[i]]++;
+            }
+            i++;
+        }
+        
+        mp.clear();
+        
+        for(char ch : s1){
+            mp[ch]++;
+        }
+        i =0;
+        while(i < s2.size()){
+            if(mp.find(s2[i]) == mp.end()){
+                ans.push_back(s2[i]);
+                mp[s2[i]]++;
+            }
+            i++;
+        }
+        
+        if(ans == " "){
+            return "-1";
+        }
+        
+        sort(ans.begin(),ans.end());     
+        return ans;
+    }
+};
+
+// Total Time Complexity = O(n+m)
+// Space Complexity = O(1)
+
