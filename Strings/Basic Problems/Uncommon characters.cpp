7c7b384fc10ef908d7604c07cfbae214f5aefaf5
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-25 16:18:44 +0530

    Update Uncommon characters.cpp

--- Strings/Basic Problems/Uncommon characters.cpp
+++ Strings/Basic Problems/Uncommon characters.cpp
@@ -2,6 +2,38 @@ class Solution {
   public:
     string uncommonChars(string& s1, string& s2) {
         string ans ="";
+        vector<int> v1(26 , 0), v2(26 , 0);
+        
+        for(int i =0;i<s1.size();i++){
+            v1[s1[i] - 'a']++;
+        }
+        
+        for(int i =0;i<s2.size();i++){
+            v2[s2[i] - 'a']++;
+        }
+        
+        for(int i =0;i<26;i++){
+            if((v1[i] > 0 && v2[i] == 0) || (v1[i] == 0 && v2[i] > 0)){
+                char ch = 'a' + i;
+                ans.push_back(ch);
+            }
+        }
+        
+        return ans;
+    }
+};
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+
+
+
+
+
+class Solution {
+  public:
+    string uncommonChars(string& s1, string& s2) {
+        string ans ="";
         unordered_map<char,int> mp;
         for(char ch : s2){
             mp[ch]++;
