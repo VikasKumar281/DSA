0a04b17cf8d797a7f30460218e84dc3e8b4924f5
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-27 19:13:29 +0530

    Create 0 - 1 Knapsack Problem(DP-19).cpp

---
+++ DP/DP on Subsequences/0 - 1 Knapsack Problem(DP-19).cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int helper(int index, int target,vector<int> &val, vector<int> &wt,  vector<vector<int>> &dp){
+        if(index == 0){
+            if(wt[0] <= target){
+                return val[0];
+            }
+            return 0;
+        }
+        
+        if(dp[index][target] != -1){
+            return dp[index][target];
+        }
+        
+        int skip = helper(index-1, target , val , wt,dp);
+        int take = INT_MIN;
+        if(wt[index] <= target){
+            take = val[index] + helper(index-1, target - wt[index],val,wt,dp);
+        }
+        
+        return dp[index][target] = max(skip , take);
+    }
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+       int n = val.size();
+       vector<vector<int>> dp(n,vector<int>(W+1,-1));
+       return helper(n-1,W,val,wt,dp);
+    }
+};
+//T.C. = O(N * W)
+//S.C. = O(N * W)
