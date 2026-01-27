9b4af6133b5025b5212350440ee69fdabaa21dac
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-27 23:38:50 +0530

    Update Rod Cutting(DP-24).cpp

--- DP/DP on Subsequences/Rod Cutting(DP-24).cpp
+++ DP/DP on Subsequences/Rod Cutting(DP-24).cpp
@@ -1,3 +1,37 @@
+// TABULATION APPROACH ------------------------------------------------------------------------------------------------->
+class Solution {
+  public:
+    int cutRod(vector<int> &price) {
+        int n = price.size();
+        int totLength = n;
+        vector<vector<int>> dp(n,vector<int>(totLength+1,0));
+        
+        for(int target=0;target <= totLength;target++){
+            dp[0][target] = target * price[0];
+        }
+        
+        for(int index = 1;index < n;index++){
+            for(int target = 0;target<= totLength;target++){
+                int skip = 0 + dp[index-1][target];
+                int take = INT_MIN;
+                int rodLength = index + 1;
+                if(rodLength <= target){
+                    take = price[index] + dp[index][target - rodLength];
+                }
+                
+                dp[index][target] = max(skip , take);
+            }
+        }
+        
+        
+        return dp[n-1][totLength];
+    }
+};
+//Time: O(n²)
+// Space: O(n²)
+
+
+
 class Solution {
   public:
     int helper(int index, vector<int> &price, vector<vector<int>> &dp,int target){
