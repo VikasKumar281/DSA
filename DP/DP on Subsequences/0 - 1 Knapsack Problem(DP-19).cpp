92fd8aded30f2366e3f3875cd85499aaa7152cd9
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-27 19:19:42 +0530

    Update 0 - 1 Knapsack Problem(DP-19).cpp

--- DP/DP on Subsequences/0 - 1 Knapsack Problem(DP-19).cpp
+++ DP/DP on Subsequences/0 - 1 Knapsack Problem(DP-19).cpp
@@ -1,3 +1,37 @@
+// TABULATION APPROACH ------------------------------------------------------------------------------------------------------------->
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+       int n = val.size();
+       vector<vector<int>> dp(n,vector<int>(W+1,0));
+       
+       for(int target = 0;target <= W;target++){
+           if(wt[0] <= target){
+               dp[0][target] = val[0];
+           }
+       }
+       
+       for(int index = 1;index<n;index++){
+           for(int target = 0;target<=W;target++){
+               int skip = dp[index-1][target];
+               int take = INT_MIN;
+               if(wt[index]<=target){
+                   take = val[index] + dp[index-1][target - wt[index]];
+               }
+               
+               dp[index][target] = max(skip , take);
+           }
+       }
+       
+       return dp[n-1][W];
+    }
+};
+// Time Complexity: O(N × W)
+// Space Complexity: O(N × W)
+
+
+
+// MEMOIZATION APPROACH ------------------------------------------------------------------------------------------------------>
 class Solution {
   public:
     int helper(int index, int target,vector<int> &val, vector<int> &wt,  vector<vector<int>> &dp){
