0544b78714cdea149ef04f43356894d2a38cd9d5
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-28 20:36:37 +0530

    Create Recursive Bubble Sort.cpp

---
+++ Sorting/Recursive Bubble Sort.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr, int n) {
+        if(n <= 1) return;
+        for(int i = 0; i < n - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+        bubbleSort(arr, n - 1);
+    }
+};
