09936526a3d6926fc155fab5766595eb015a998a
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-29 16:08:42 +0530

    Update Median of Two Sorted Arrays.cpp

--- Binary Search/Median of Two Sorted Arrays.cpp
+++ Binary Search/Median of Two Sorted Arrays.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> temp;
         int n = nums1.size();
         int m = nums2.size();
-
-        vector<int> v;
-        for(int i =0;i<n;i++){
-            v.push_back(nums1[i]);
-        }
-        for(int i =0;i<m;i++){
-            v.push_back(nums2[i]);
-        }
-
-        sort(v.begin(),v.end());
         
-        int a = v.size();
-
-        if (a % 2 == 0) {
-            return (v[a/2 - 1] + v[a/2]) / 2.0;
-        } else {
-            return v[a/2];
+        int i =0,j=0;
+        while(i<n && j<m){
+            if(nums1[i] <= nums2[j]){
+                temp.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                temp.push_back(nums2[j]);
+                j++;
+            }
         }
+        while(i<n){
+            temp.push_back(nums1[i]);
+            i++;
+        }
+        while(j<m){
+            temp.push_back(nums2[j]);
+            j++;
+        }
+
+        int total = n+m;
+
+        if(total % 2 == 1){
+            return temp[total/2];
+        }
+        
+        return (temp[total/2 - 1] + temp[total/2]) / 2.0;
+
 
     }
 };
-// Time: O((n+m) log(n+m))
+// Time: O((n+m)
 // Space: O(n+m)
