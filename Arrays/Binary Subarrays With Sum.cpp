8140c726612071e3586440db73104e89c988a77c
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-30 17:25:39 +0530

    Update Binary Subarrays With Sum.cpp

--- Arrays/Binary Subarrays With Sum.cpp
+++ Arrays/Binary Subarrays With Sum.cpp
@@ -1,3 +1,25 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        
+        for(int x : nums){
+            sum += x;
+            if(mp.count(sum - goal)){
+                count += mp[sum - goal];
+            }
+            
+            mp[sum]++;
+        }
+        
+        return count;
+    }
+};
+// Time complexity = O(n).
+// Space complexity = O(n).
+
 
 // TLE Shows on LeetCode
 class Solution {
