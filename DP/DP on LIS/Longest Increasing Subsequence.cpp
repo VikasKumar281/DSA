a9cbc8a1603dfd4529e6f4c21ff1f28a78621237
Vikas Kumar <vikaskumar280204@gmail.com>
2026-01-30 19:48:10 +0530

    Update Longest Increasing Subsequence.cpp

--- DP/DP on LIS/Longest Increasing Subsequence.cpp
+++ DP/DP on LIS/Longest Increasing Subsequence.cpp
@@ -1,3 +1,28 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = 1;
+
+        vector<int> dp(n,1);
+        for(int i = 0;i<n;i++){
+            for(int prev = 0; prev < i;prev++){
+                if(nums[prev] < nums[i]){
+                    dp[i] = max(dp[i] , 1 + dp[prev]);
+                }
+            }
+
+            maxi = max(maxi , dp[i]);
+        }
+
+        return maxi;
+    }
+};
+// Time = O(n²)
+// Space = O(n)
+
+
+
 // TABULATION APPROACH -------------------------------------------------------------------------------------------------------------->
 class Solution {
   public:
