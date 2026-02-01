4363256e8271e617b2982c68d895d220fc9771c4
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-01 17:54:15 +0530

    Add recursive function for letter combinations
    Implement recursive solution to generate letter combinations for a given phone number using a mapping of digits to letters.

--- Recursion/Letter Combinations of a Phone Number.cpp
+++ Recursion/Letter Combinations of a Phone Number.cpp
@@ -1,4 +1,57 @@
 class Solution {
+  public:
+    void solve(int idx, vector<int> &arr, unordered_map<int, string> &mp, vector<string> &result, string &temp){
+        if(idx >= arr.size()){
+            result.push_back(temp);
+            return;
+        }
+        
+        int ch = arr[idx];
+        string str = mp[ch];
+        
+        if(mp[ch].empty()){
+          solve(idx + 1, arr, mp, result, temp);
+          return;
+        } 
+            
+        for(int i =0;i<str.length();i++){
+            temp.push_back(str[i]);
+            solve(idx+1, arr, mp, result, temp);
+            temp.pop_back();
+        }
+    }
+    vector<string> possibleWords(vector<int> &arr) {
+        vector<string> result;
+        if(arr.size() == 0){
+            return result;
+        }
+        
+        unordered_map<int,string> mp;
+        mp[0] = "";
+        mp[1] = "";
+        mp[2] = "abc";
+        mp[3] = "def";
+        mp[4] = "ghi";
+        mp[5] = "jkl";
+        mp[6] = "mno";
+        mp[7] = "pqrs";
+        mp[8] = "tuv";
+        mp[9] = "wxyz";
+        
+        string temp = "";
+        
+        solve(0, arr, mp, result, temp);
+        
+        return result;
+    }
+};
+// Time Complexity: O(4^k), where k is the count of digits that map to letters.
+// Space Complexity: O(n)
+
+
+
+
+class Solution {
 public:
     void solve(int idx, string &digits,  unordered_map<char,string>&mp, vector<string> &result, string &temp){
         if(idx >= digits.length()){
