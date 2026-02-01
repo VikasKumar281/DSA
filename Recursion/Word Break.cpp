484947544d787b27791c460ec565095ce77dab09
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-02 00:03:12 +0530

    Add memoized recursion for Word Break solution
    Implemented a recursive solution with memoization for the Word Break problem, optimizing the original approach.

--- Recursion/Word Break.cpp
+++ Recursion/Word Break.cpp
@@ -1,3 +1,48 @@
+// RECURSION WITH MEMOIZATION ------------------------------------------------------------------------------>
+class Solution {
+public:
+    int n;
+    unordered_set<string> st;
+    int t[301];
+
+    bool solve(int index , string &s){
+        if(index >= n){
+            return true;
+        }
+        
+        if(t[index] != -1){
+            return t[index];
+        }
+
+        for(int len = 1; len <= n - index ; len++){
+            string temp = s.substr(index,len);
+
+            if(st.find(temp) != st.end() && solve(index+len , s)){
+                return t[index] = true;
+            }
+        }
+
+        return t[index] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict){
+        n = s.length();
+        memset(t, -1, sizeof(t));
+        st.clear();
+
+        for(string &word : wordDict){
+            st.insert(word);
+        }
+
+        return solve(0 , s);
+    }
+};
+// Time: O(n²)
+// Space: O(n)
+
+
+
+
+
 // RECURSION WITHOUT MEMOIZATION ------------------------------------------------------------------------------>
 class Solution {
 public:
