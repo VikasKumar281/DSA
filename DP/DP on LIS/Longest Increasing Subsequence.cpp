6ad494316add7138cd7acf6a9886831bc16049cc
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-03 20:48:33 +0530

    Add binary search approach for Longest Increasing Subsequence
    Implemented the Longest Increasing Subsequence algorithm using binary search for efficiency.

--- DP/DP on LIS/Longest Increasing Subsequence.cpp
+++ DP/DP on LIS/Longest Increasing Subsequence.cpp
@@ -1,3 +1,29 @@
+// LIS USING BINARY SEARCH --------------------------------------------------------------------------------->
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> temp;
+        temp.push_back(nums[0]);
+
+        for(int i =1;i<n;i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int index = lower_bound(temp.begin(),temp.end() ,nums[i]) - temp.begin();
+                temp[index] = nums[i];
+            }
+        }
+
+        return temp.size();
+    }
+};
+// Time Complexity => O(N∗Log(N))
+// Space Complexity => O(N)
+
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
