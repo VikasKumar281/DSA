28b3ecb4ba835cd0e5225c6bb1c4bc3d863043d7
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-08 18:31:36 +0530

    Create Book Allocation Problem(Allocate Minimum Pages).cpp

---
+++ Binary Search/Book Allocation Problem(Allocate Minimum Pages).cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    bool isAllocationPossible(vector<int> &arr, int student , int pages){
+        int n = arr.size();
+        int stu = 1;
+        int sum =0;
+        for(int i =0;i<n;i++){
+            if(arr[i] > pages){
+                return false;
+            }
+            if(sum + arr[i] <= pages){
+                sum += arr[i];
+            }
+            else{
+                stu++;
+                sum = arr[i];
+            }
+        }
+        
+        if(stu <= student){
+            return true;
+        }
+        
+        return false;
+    } 
+    int findPages(vector<int> &arr, int k) {
+        if(k > arr.size()){
+            return -1;
+        }
+        int low = *min_element(arr.begin(),arr.end());
+        int high = accumulate(arr.begin(),arr.end(),0);
+        
+        int res = 0;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isAllocationPossible(arr, k,mid)){
+                res = mid;
+                high = mid - 1; 
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return res;
+    }
+};
