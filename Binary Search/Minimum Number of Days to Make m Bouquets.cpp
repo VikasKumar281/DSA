7bfe2c82d8187b02670352abbe89170c7acf206d
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-08 17:36:25 +0530

    Update Minimum Number of Days to Make m Bouquets.cpp

--- Binary Search/Minimum Number of Days to Make m Bouquets.cpp
+++ Binary Search/Minimum Number of Days to Make m Bouquets.cpp
@@ -21,6 +21,64 @@ public:
         return false;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
+        int mini = INT_MAX; 
+        int maxi = INT_MIN; 
+        
+        for(int i =0;i<bloomDay.size();i++){
+            mini = min(mini , bloomDay[i]);
+            maxi = max(maxi, bloomDay[i]);
+        }
+        
+        int low = mini;
+        int high = maxi;
+
+        if((long long)bloomDay.size() < (long long)m * k){
+            return -1;
+        }
+
+        int ans = high;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isPossible(bloomDay,m,k,mid)){
+                ans = mid;
+                high = mid - 1;
+            } 
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+// Time Complexity=> O(N∗Log(Max−Min))
+// Space Complexity=> O(1)
+
+
+
+class Solution {
+public:
+    bool isPossible(vector<int>& bloomDay , int m, int k, int days){
+        int count =0 , bouquets = 0;
+
+        for(int i =0 ; i < bloomDay.size();i++){
+            if(bloomDay[i] <= days){
+                count++;
+            }
+            else{
+                bouquets += (count/k);
+                count = 0;
+            }
+        }
+        bouquets += (count/k);
+
+        if(bouquets >= m){
+            return true;
+        }
+
+        return false;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
         int low = *min_element(bloomDay.begin(),bloomDay.end());
         int high = *max_element(bloomDay.begin(),bloomDay.end());
 
@@ -43,3 +101,5 @@ public:
         return ans;
     }
 };
+// Time Complexity=> O(N∗Log(Max−Min))
+// Space Complexity=> O(1)
