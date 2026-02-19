cc59a5966fad80d814c1e1ba20552541553dc268
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-19 20:26:53 +0530

    Update Binary Number with Alternating Bits.cpp

--- LeetCode DCC/Binary Number with Alternating Bits.cpp
+++ LeetCode DCC/Binary Number with Alternating Bits.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    int highestBit(int n){
+        return (int)log2(n);
+    }
+    bool hasAlternatingBits(int n) {
+        int highestBitset = highestBit(n);
+
+        bitset<32>b(n);
+        for(int i =0;i <= highestBitset;i++){
+            if(b[i] == b[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+class Solution {
+public:
     bool hasAlternatingBits(int n) {
        int prev = n % 2;
         n /= 2;
