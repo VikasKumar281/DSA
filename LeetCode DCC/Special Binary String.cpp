5568a5e18730793ac85cd3db3cda733a116a1bf7
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-20 23:46:39 +0530

    Create Special Binary String.cpp

---
+++ LeetCode DCC/Special Binary String.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> parts;
+        int count = 0, start = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') {
+                count++;
+            }
+            else{
+                count--;
+            }
+
+            if(count == 0) {
+                string inner = makeLargestSpecial(s.substr(start + 1, i - start - 1));
+                parts.push_back("1" + inner + "0");
+                start = i + 1;
+            }
+        }
+
+        sort(parts.rbegin(), parts.rend());
+
+        string result = "";
+        for(string &p : parts) result += p;
+
+        return result;
+    }
+};
