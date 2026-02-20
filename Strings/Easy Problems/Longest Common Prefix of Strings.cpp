eab028e131bf42d4f81cda8b60d4d2e2b7139da3
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-20 23:36:51 +0530

    Update Longest Common Prefix of Strings.cpp

--- Strings/Easy Problems/Longest Common Prefix of Strings.cpp
+++ Strings/Easy Problems/Longest Common Prefix of Strings.cpp
@@ -1,3 +1,36 @@
+// USING VERTICAL SCANNING METHOD -------------------------------------------------------------------------------------------------->
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+
+        for(int i =0;i<strs[0].size();i++){
+            char ch = strs[0][i];
+
+            bool match = true;
+
+            for(int j = 1;j<strs.size();j++){
+                if(strs[j].size() < i || ch != strs[j][i]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match == false){
+                break;
+            }
+            else{
+                ans.push_back(ch);
+            }
+        }
+
+        return ans;
+    }
+
+};
+//Time Complexity => O(N × M)
+
+
+
 class Solution {
   public:
     string longestCommonPrefix(vector<string> arr) {
@@ -17,6 +50,7 @@ class Solution {
         temp.push_back(s1[i]);
         i++;
       }
+//Time Complexity => O(NlogN) 
       
       return temp;
     }
