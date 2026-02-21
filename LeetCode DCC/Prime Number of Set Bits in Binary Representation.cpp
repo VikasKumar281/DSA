77f238f3ae071eedb6147eac597dfebb7c011586
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-21 17:03:43 +0530

    Update Prime Number of Set Bits in Binary Representation.cpp

--- LeetCode DCC/Prime Number of Set Bits in Binary Representation.cpp
+++ LeetCode DCC/Prime Number of Set Bits in Binary Representation.cpp
@@ -16,3 +16,36 @@ public:
         return result;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int countSetBits(int num){
+       int count = 0; 
+       while(num > 0){
+        if(num % 2 == 1){
+            count++;
+        }
+        num = num/2;
+       }
+       return count;
+    }
+    
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19};
+
+        int result = 0;
+
+        for(int num = left;num<= right;num++){
+            int setBits = countSetBits(num);
+
+            if(primes.count(setBits)){
+                result++;
+            }
+        }
+
+        return result;
+    }
+};
