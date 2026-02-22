28c929a90a1818225a16b6563797c6e02162a2c4
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-22 18:30:11 +0530

    Update Max Consecutive Ones III.cpp

--- Sliding Window & Two Pointer Combined Problems/Max Consecutive Ones III.cpp
+++ Sliding Window & Two Pointer Combined Problems/Max Consecutive Ones III.cpp
@@ -1,3 +1,36 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxLen = 0;
+        
+        int zeroes = 0;
+        int left = 0, right = 0;
+        
+        while(right < nums.size()){
+           if(nums[right] == 0){
+             zeroes++;
+           }
+           while(zeroes > k){
+            if(nums[left] == 0){
+                zeroes--;
+            }
+            left++;
+           }
+           if(zeroes <= k){
+            maxLen = max(maxLen, right - left + 1);
+           }
+           right++;
+        }
+
+        return maxLen;
+    }
+};
+// T.C. = O(N)
+// S.C. = O(1)
+
+
+
 // This Method Gives TLE ------------------------------------------------------------------------------->
 class Solution {
 public:
