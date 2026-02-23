9cb9f84024dc4c0e2d40c377b01b7734a4151bcb
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-23 17:48:46 +0530

    Update Sorting Employees.cpp

--- Strings/Easy Problems/Sorting Employees.cpp
+++ Strings/Easy Problems/Sorting Employees.cpp
@@ -1,6 +1,41 @@
 class Solution {
   public:
     vector<string> sortRecords(vector<string>& employee, vector<int>& salary) {
+        int n1 = employee.size();
+        int n2 = salary.size();
+        
+        vector<pair<int,string>> arr;
+        
+        for(int i =0;i<n1;i++){
+            arr.push_back({salary[i], employee[i]});
+        }
+        
+        sort(arr.begin(),arr.end() , [](auto &a, auto &b){ 
+           if(a.first == b.first){
+               return a.second < b.second; 
+           }
+           return a.first < b.first;
+        });
+        
+        vector<string> result;
+        
+        for(auto &p : arr){
+            result.push_back(p.second);
+        }
+        
+        
+        return result;
+    }
+};
+//T.C. => O(n log n)
+//S.C. => O(n)
+
+
+
+
+class Solution {
+  public:
+    vector<string> sortRecords(vector<string>& employee, vector<int>& salary) {
         int n = employee.size();
         
         for(int i = 0; i < n - 1; i++) {
