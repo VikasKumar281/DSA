e2dcfb34eb51ac5b347e21de6a048d349777a383
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-23 16:03:51 +0530

    Create Check If a String Contains All Binary Codes of Size K.cpp

---
+++ LeetCode DCC/Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int pow(int a , int k){
+        int res = 1;
+        for(int i =0;i<k;i++){
+           res = res * a;
+        }
+
+        return res;
+    }
+    bool hasAllCodes(string s, int k) {
+        // totalcodes = 2 ^ k
+        int n = s.size();
+
+        unordered_set<string> st;
+
+        for(int i = k;i<=n;i++){
+           st.insert(s.substr(i-k,k));
+        }
+
+        int n2 = st.size();
+        if(n2 == pow(2,k)){
+            return true;
+        }
+
+        return false;
+    }
+};
