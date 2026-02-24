57cf6e1949fb546ae6b17785c627a2aa4438845e
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-24 20:49:01 +0530

    Update Longest Repeating Character Replacement.cpp

--- Sliding Window & Two Pointer Combined Problems/Longest Repeating Character Replacement.cpp
+++ Sliding Window & Two Pointer Combined Problems/Longest Repeating Character Replacement.cpp
@@ -3,7 +3,42 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.size();
         
-        int left = 0 , right = 0, maxLen = 0;
+        vector<int>freq(26,0);
+        int maxfreq = 0, maxLen = 0;
+        int left = 0 , right = 0;
+        
+        while(right < n){
+          freq[s[right] - 'A']++;
+          maxfreq = max(maxfreq , freq[s[right] - 'A']);
+          
+          int change = (right - left + 1) - maxfreq; 
+          
+          while(change > k){   
+            freq[s[left]-'A']--;
+            left++;
+            change = (right - left + 1) - maxfreq;
+          } 
+
+          maxLen = max(maxLen , right - left + 1);
+          right++;
+        }
+
+        return maxLen;
+    }
+};
+//T.C. = O(N)
+//S.C. = O(1)
+
+
+
+
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        
+        int maxLen = 0;
         
         for(int i = 0;i<n;i++){
            vector<int>freq(26,0);
@@ -26,4 +61,4 @@ public:
     }
 };
 //T.C. = O(N^2)
-//S.C. = O(N)
+//S.C. = O(1)
