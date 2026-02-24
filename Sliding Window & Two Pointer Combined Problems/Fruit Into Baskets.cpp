db8d161008bfa50c5ede8fc210754cfb0a79adaf
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-24 15:52:41 +0530

    Update Fruit Into Baskets.cpp

--- Sliding Window & Two Pointer Combined Problems/Fruit Into Baskets.cpp
+++ Sliding Window & Two Pointer Combined Problems/Fruit Into Baskets.cpp
@@ -2,6 +2,40 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
+        unordered_map<int,int> mp;
+
+        int k = 2; // 2 baskets.
+        int left = 0, right = 0, maxLen = 0;
+        while(right < n){
+            mp[fruits[right]]++;
+            if(mp.size() > k){
+                mp[fruits[left]]--;
+                if(mp[fruits[left]] == 0){
+                    mp.erase(fruits[left]);
+                }
+                left++;
+            }
+            if(mp.size() <= k){
+                maxLen = max(maxLen, right - left + 1);
+            }
+
+            right++;
+        }
+
+        return maxLen;
+    }
+};
+//T.C. = O(N)
+//S.C. = O(1)
+
+
+
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
         
         int maxLen = 0;
         for(int i =0;i<n;i++){
