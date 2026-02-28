af87a553f16f01bb5cd272869165a431e2c1ebfb
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-28 17:29:17 +0530

    Create Concatenation of Consecutive Binary Numbers.cpp

---
+++ LeetCode DCC/Concatenation of Consecutive Binary Numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int mod = 1e9 + 7;
+        long long result = 0;
+        int bitLength = 0;
+
+        for(int i = 1;i <= n; i++) {
+            if((i &(i -1))==0){
+                bitLength++;
+            }
+            result =((result <<bitLength)+i) %mod;
+        }
+
+        return result;
+    }
+};
