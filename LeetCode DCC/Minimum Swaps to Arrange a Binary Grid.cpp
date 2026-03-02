aaeb1ba650c4092ee35a1545eef396246ee7c72c
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-02 23:34:17 +0530

    Create Minimum Swaps to Arrange a Binary Grid.cpp

---
+++ LeetCode DCC/Minimum Swaps to Arrange a Binary Grid.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> trailing(n);
+
+        //Step 1 => Count trailing zeros ---------------------->
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            for(int j = n - 1; j >= 0; j--) {
+                if(grid[i][j] == 0){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            trailing[i] = count;
+        }
+
+        int swaps = 0;
+
+        // Step 2 => Fix each row --------------------------------------------->
+        for(int i = 0; i < n; i++) {
+            int required = n - i - 1;
+            int j = i;
+
+            while(j < n &&trailing[j]<required) {
+                j++;
+            }
+
+            if(j== n){
+                return -1;
+            }
+
+            // Bubble up
+            while(j > i) {
+                swap(trailing[j], trailing[j - 1]);
+                swaps++;
+                j--;
+            }
+        }
+
+        return swaps;
+    }
+};
