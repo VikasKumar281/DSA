d9d20a38e9b201518e626e4a4c66bd857df1856a
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-12 16:35:53 +0530

    Add longestKSubstr function for K unique characters
    Implemented a function to find the longest substring with exactly K unique characters using a sliding window approach.

--- Sliding Window & Two Pointer Combined Problems/Longest Substring with K Uniques.cpp
+++ Sliding Window & Two Pointer Combined Problems/Longest Substring with K Uniques.cpp
@@ -3,6 +3,38 @@ class Solution {
     int longestKSubstr(string &s, int k) {
         int n = s.size();
         
+        unordered_map<char, int> mpp;
+        
+        int left = 0, maxLen = -1, right = 0;
+        while(right<n){
+            mpp[s[right]]++;
+            if(mpp.size() == k){
+                maxLen = max(maxLen , right-left+1);
+            }
+            while(mpp.size() > k){
+                mpp[s[left]]--;
+                if(mpp[s[left]] == 0){
+                    mpp.erase(s[left]);
+                }
+                left++;
+            }
+            right++;
+        }
+        
+        
+        return maxLen;
+    }
+};
+//T.C. = O(N)
+//S.C. = O(1)
+
+
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        int n = s.size();
+        
         unordered_map<char,int> mp;
         int maxlen = -1;
         
