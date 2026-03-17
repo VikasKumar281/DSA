8645f065f4ebed2e32c4d6077f6ea2baac8e59bb
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-17 18:41:49 +0530

    Update Count Subarrays with given XOR.cpp

--- Arrays/Count Subarrays with given XOR.cpp
+++ Arrays/Count Subarrays with given XOR.cpp
@@ -1,5 +1,28 @@
-
-
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        int n = arr.size();
+        map<int,int> mpp;
+        int xr = 0;
+        
+        mpp[xr]++;
+        
+        int count =0;
+        
+        for(int i =0;i<n;i++){
+            xr = xr ^ arr[i];
+            
+            int x = xr ^ k;
+            count += mpp[x];
+            
+            mpp[xr]++;
+        }
+        
+        return count;
+    }
+};
+//T.C. = O(N)
+//S.C. = O(N)
 
 
 
