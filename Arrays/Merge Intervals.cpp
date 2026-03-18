e53e3b625e1f6e09ebd6324b8e08bb6e430023ff
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-18 16:32:18 +0530

    Update Merge Intervals.cpp

--- Arrays/Merge Intervals.cpp
+++ Arrays/Merge Intervals.cpp
@@ -1,3 +1,32 @@
+// OPTIMAL APPROACH ------------------------------------------------------------------------------------------------------------->
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+        
+        for(int i =0;i<n;i++){
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+        
+        return ans;
+    }
+};
+//T.C. = O(N logN) + O(N)
+//S.C. = O(N)
+
+
+
+
+// BRUTE-FORCE APPROACH ------------------------------------------------------------------------------------------------------------->
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
