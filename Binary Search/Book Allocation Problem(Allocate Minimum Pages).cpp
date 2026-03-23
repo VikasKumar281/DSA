8cb0a0fd38cb091f133c718e39306288a4914554
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-23 11:01:26 +0530

    Update Book Allocation Problem(Allocate Minimum Pages).cpp

--- Binary Search/Book Allocation Problem(Allocate Minimum Pages).cpp
+++ Binary Search/Book Allocation Problem(Allocate Minimum Pages).cpp
@@ -1,5 +1,61 @@
 class Solution {
   public:
+    bool allocationPossible(int barrier, vector<int> &arr, int k){
+        int allocatedStudents = 1, pages = 0;
+        
+        for(int i =0;i< arr.size();i++){
+            if(arr[i] > barrier){
+                return false;
+            }
+            if(pages + arr[i] > barrier){
+                allocatedStudents++;
+                pages = arr[i];
+            }
+            else{
+                pages += arr[i];
+            }
+        }
+        
+        if(allocatedStudents > k){
+            return false;
+        }
+        
+        return true;
+    }
+    int findPages(vector<int> &arr, int k) {
+       int n = arr.size();
+       
+       if(k > n){
+           return -1;
+       }
+
+       int low = *max_element(arr.begin(), arr.end()); // Gives Maximum element of arr.
+       int high = accumulate(arr.begin(), arr.end(), 0); // Gives Total Sum of elements of arr.
+       
+       int res = -1;
+       
+       while(low <= high){
+           int mid = low + (high - low)/2;
+           
+           if(allocationPossible(mid, arr, k)){
+               res = mid;
+               high = mid-1;
+           }
+           else{
+               low = mid + 1;
+           }
+       }
+       
+       return res;
+    }
+};
+
+
+
+
+
+class Solution {
+  public:
     bool isAllocationPossible(vector<int> &arr, int student , int pages){
         int n = arr.size();
         int stu = 1;
