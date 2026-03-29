c98632d2a1d2f97642ba00670ba664d46c5886fc
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-29 13:59:06 +0530

    Update Check if Strings Can be Made Equal With Operations I.cpp

--- LeetCode DCC/Check if Strings Can be Made Equal With Operations I.cpp
+++ LeetCode DCC/Check if Strings Can be Made Equal With Operations I.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
+        if(s1 == s2){
+            return true;
+        }
+
+        if((s1[0] == s2[2] && s1[2] == s2[0]) &&
+            s1[1] == s2[3] && s1[3] == s2[1]){
+                return true;
+        }
+
+        if((s1[0] == s2[0] && s1[2] == s2[2]) &&
+            s1[1] == s2[3] && s1[3] == s2[1]){
+                return true;
+        }
+
+        if((s1[0] == s2[2] && s1[2] == s2[0]) &&
+            s1[1] == s2[1] && s1[3] == s2[3]){
+                return true;
+        }
+
+        return false;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
         int i = 0;
         int j = 2;
         while(i < 4 && j < 4){
