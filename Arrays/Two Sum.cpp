258db279bd0d21539949767895694e2e610c3f54
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-30 19:39:18 +0530

    Update Two Sum.cpp

--- Arrays/Two Sum.cpp
+++ Arrays/Two Sum.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
        int n = nums.size();
+       unordered_map<int,int> mp;
+
+       for(int i =0;i<n;i++){
+        int rem = target - nums[i];
+        if(mp.count(rem)){
+            return{i,mp[rem]};
+        }
+
+        mp[nums[i]] = i;
+       }
+
+       return {};
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int n = nums.size();
        unordered_map<int,int>mp;
      
        for(int i=0;i<n;i++){
