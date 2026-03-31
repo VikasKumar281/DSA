de75f173674a826e11c096e5d672615b01664a60
Vikas Kumar <vikaskumar280204@gmail.com>
2026-03-31 11:35:50 +0530

    Update Count Commas in Range.cpp

--- Count Commas in Range.cpp
+++ Count Commas in Range.cpp
@@ -4,6 +4,17 @@ public:
         if(n < 1000){
             return 0;
         }
+        return (n - 999);
+    }
+};
+
+
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n < 1000){
+            return 0;
+        }
         return (n - 1000 + 1);
     }
 };
