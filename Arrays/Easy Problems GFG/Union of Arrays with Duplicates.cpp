8f1ac0684173da0cdd87214759b9fbced907d8e6
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-04 13:08:30 +0530

    Update Union of Arrays with Duplicates.cpp

--- Arrays/Easy Problems GFG/Union of Arrays with Duplicates.cpp
+++ Arrays/Easy Problems GFG/Union of Arrays with Duplicates.cpp
@@ -1,4 +1,29 @@
 class Solution {
+public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        unordered_set<int> st;
+
+        for(int x : a){
+          st.insert(x);
+        }
+        for(int x : b){
+          st.insert(x);
+        }
+
+        vector<int> res(st.begin(), st.end());
+      
+        sort(res.begin(), res.end());
+
+        return res;
+    }
+};
+// Time: O(n + m) average
+// Space: O(n + m)
+
+
+
+
+class Solution {
   public:
     vector<int> findUnion(vector<int>& a, vector<int>& b) {
       set<int> st;
