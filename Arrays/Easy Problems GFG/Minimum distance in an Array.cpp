230b3e7766559655311dece4c8927c8a5a187699
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-07 16:36:48 +0530

    Create Minimum distance in an Array.cpp

---
+++ Arrays/Easy Problems GFG/Minimum distance in an Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int minDist(vector<int>& arr, int x, int y) {
+        int n = arr.size();
+        
+        int mini = INT_MAX;
+        int idx1 = -1, idx2 = -1; 
+        for(int i =0;i<n;i++){
+            if(arr[i] == x){
+                idx1 = i;
+            }
+            else if(arr[i] == y){
+                idx2 = i;
+            }
+            
+            if(idx1 !=-1 && idx2 != -1){
+              mini = min(mini, abs(idx2-idx1));
+            }
+        }
+        
+        if(mini == INT_MAX){
+            return -1;
+        }
+        
+        return mini;
+    }
+};
