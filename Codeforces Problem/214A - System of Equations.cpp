fb9885648aa79e7f8a130f410268a495f35eefa0
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-08 20:59:41 +0530

    Create 	214A - System of Equations.cpp

---
+++ Codeforces Problem/214A - System of Equations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int cnt = 0;
+
+    for (int a = 0; a <= 1000; a++) {
+        for (int b = 0; b <= 1000; b++) {
+            if (a * a + b == n && a + b * b == m) {
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+}
