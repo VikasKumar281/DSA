988a7ed6dde05b64a0bfb26d63e90537a0ffbbc0
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-12 18:28:28 +0530

    Create Matrix Diagonal Sum.cpp

---
+++ Arrays/Matrix Diagonal Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+       int sum = 0;
+       int m = mat.size();
+       int n = mat[0].size();
+
+       for(int i =0;i<m;i++){
+        for(int j = 0;j<n;j++){
+           if(i == j){
+            sum += mat[i][j];
+           }
+        }
+       } 
+        
+        int col = n-1;
+        for(int i =0;i<m;i++){
+            if(i == col){
+                col--;
+                continue;
+            }
+            if(col >= 0){
+              sum += mat[i][col];
+              col--;
+            }
+        } 
+
+
+       return sum;
+    }
+};
