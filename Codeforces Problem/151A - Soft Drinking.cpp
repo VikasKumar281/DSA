45a180a4c731e5a757ee6ecd4eeae00f935acce6
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-16 21:53:45 +0530

    Create 151A - Soft Drinking.cpp

---
+++ Codeforces Problem/151A - Soft Drinking.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+
+    int drink = (k * l) / nl;
+    int lime = c * d;
+    int salt = p / np;
+
+    int ans = min({drink, lime, salt}) / n;
+
+    cout << ans;
+
+    return 0;
+}
