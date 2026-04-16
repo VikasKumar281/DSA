36601b64ddcbc7bde09bee6b7b8b21cf8a151e3c
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-16 17:54:46 +0530

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -1,3 +1,73 @@
+//T.C. = O(N logN) --------------------------------------------->
+//S.C. = O(N)
+class Solution {
+  public:
+    void merge(vector<int> &arr,int l , int mid, int r){
+        int m = mid - l + 1;
+        // (r-1) - (mid+1) => r - m
+        int n = r - mid;
+        
+        vector<int> left(m), right(n);
+        
+        int k = l;
+        
+        for(int i =0;i<m;i++){
+            left[i] = arr[k];
+            k++;
+        }
+        
+        for(int j =0;j<n;j++){
+            right[j] = arr[k];
+            k++;
+        }
+        
+        // Merge Both Arrays -------------------->
+        int i = 0, j = 0;
+        k = l;
+        
+        while(i < m && j < n){
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            }
+            else{
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i < m){
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        
+        while(j < n){
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+    void solve(vector<int> &arr, int l,int r){
+        if(l >= r){
+            return;
+        }
+        int mid = l + (r - l)/2;
+        
+        solve(arr, l, mid);
+        solve(arr, mid+1, r);
+        
+        merge(arr, l, mid, r);
+    }
+    void mergeSort(vector<int>& arr, int l, int r){
+        solve(arr, l, r);
+    }
+};
+
+
+
+
 class Solution {
   public:
     void merge(vector<int> &arr , int low ,int mid, int high){
