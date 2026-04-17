3617cb5d4e604f604ec7f5ab8ec36f51e7a8020b
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-17 12:53:30 +0530

    Update Quick Sort.cpp

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -1,3 +1,12 @@
+//Approach - Using Quick Sort , sort the array
+// T.C. = O(N logN) => (Best Case & Average Case)
+// T.C. = O(N^2) => (Worst Case)
+
+// S.C. ====================>
+// Average : O(log N) (Recursion Stack used)
+// Worst : O(N)
+// Extra space : O(1)
+
 class Solution {
   public:
     void quickSort(vector<int>& arr, int low, int high) {
