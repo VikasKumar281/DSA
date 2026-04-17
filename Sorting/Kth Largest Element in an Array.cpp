7b208d40def38ca165fa371a53b06c943975ed57
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-17 21:43:50 +0530

    Update Kth Largest Element in an Array.cpp

--- Sorting/Kth Largest Element in an Array.cpp
+++ Sorting/Kth Largest Element in an Array.cpp
@@ -1,3 +1,26 @@
+//Approach => Used Min Heap ------------------------------------------------------------> 
+// T.C. =>O(N log K)
+// S.C => O(K)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+       priority_queue<int, vector<int>, greater<int>> pq; // MinHeap
+       
+       for(int &num : nums){
+           pq.push(num);
+           
+           if(pq.size() > k){
+               pq.pop();
+           }
+       }
+       
+       return pq.top(); 
+    }
+};
+
+
+
+
 //Approach - Used QuickSelect Algorithm
 // Time Complexity =>
 //    - Best Case: O(N)
