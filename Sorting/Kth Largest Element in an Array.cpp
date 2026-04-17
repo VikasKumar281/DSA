68c057b7c84742bc9b9b9a81858811485910c50e
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-17 21:31:57 +0530

    Update Kth Largest Element in an Array.cpp

--- Sorting/Kth Largest Element in an Array.cpp
+++ Sorting/Kth Largest Element in an Array.cpp
@@ -1,4 +1,68 @@
+//Approach - Used QuickSelect Algorithm
+// Time Complexity =>
+//    - Best Case: O(N)
+// Jab pivot perfect split kare (array roughly half-half divide ho)
+//    - Average Case: O(N)
+//    - Worst Case: O(N²)
+// Jab pivot hamesha smallest ya largest nikle
+// (example: already sorted array + bad pivot choice)
 
+// Space Complexity =>
+// Iterative version : O(1) 
+class Solution {
+public:
+    int partition(vector<int> &nums, int left, int right){
+        int pivot = nums[left];
+        int i = left + 1;
+        int j = right;
+
+        while(i <= j){
+            if(nums[i] < pivot && nums[j] > pivot){
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+
+            if(nums[i] >= pivot){
+                i++;
+            }
+
+            if(nums[j] <= pivot){
+                j--;
+            }
+        } 
+
+        swap(nums[left], nums[j]);
+
+        return j;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int left = 0;
+        int right = n-1;
+
+        int pivotIdx = 0;
+
+        //Kth Largest Pivot Element => nums[k-1] (Jab Descending Order me partition hoga) -------->
+
+        while(true){
+            pivotIdx = partition(nums, left, right);
+
+            if(pivotIdx == k-1){
+                break;
+            }
+            else if(pivotIdx > k-1){
+                right = pivotIdx - 1;
+            }
+            else{
+                left = pivotIdx + 1;
+            }
+        } 
+
+        return nums[pivotIdx];
+    }
+};
 
 // Time Limit Exceeded ---------------------------------------------------------------->
 //Approach - Using Quick Sort , sort the array
