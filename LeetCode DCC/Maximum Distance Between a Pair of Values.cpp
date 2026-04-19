ddd9987a3c10c13d483322eb1a0aedcfe0cab657
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-19 18:29:32 +0530

    Create Maximum Distance Between a Pair of Values.cpp

---
+++ LeetCode DCC/Maximum Distance Between a Pair of Values.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+       int m = nums1.size(); 
+       int n = nums2.size();
+
+       int maxi = 0;
+
+       int i =0, j=0;
+       while(i < m && j < n){
+        if(nums1[i] > nums2[j]){
+            i++;
+        }
+        else{
+          maxi = max(maxi, j - i);
+          j++;
+        }
+       } 
+
+       return maxi;
+    }
+};
