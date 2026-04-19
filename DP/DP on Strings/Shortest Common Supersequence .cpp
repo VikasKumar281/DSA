c979b7ba4ee897b55dc89f4fda888df45f8875ab
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-19 16:54:27 +0530

    Create Shortest Common Supersequence .cpp

---
+++ DP/DP on Strings/Shortest Common Supersequence .cpp
@@ -0,0 +1,32 @@
+// Approach => Memoization + Recursion ---------------------------------------------------------------------->
+// Time: O(n1 * n2)
+// Space: O(n1 * n2)
+class Solution {
+  public:
+    int solve(int i , int j , string &s1, string &s2, vector<vector<int>> &dp){
+        if( i >= s1.size()){
+            return s2.size() - j;
+        }
+        else if(j >= s2.size()){
+            return s1.size() - i;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 1 + solve(i+1, j+1, s1, s2, dp);
+        }
+        
+        return dp[i][j] = 1 + min(solve(i+1, j, s1,s2,dp) , solve(i, j+1, s1, s2, dp));
+    }
+    int minSuperSeq(string &s1, string &s2) {
+     int n1 = s1.size();
+     int n2 = s2.size();
+     
+     vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+     
+     return solve(0, 0, s1, s2, dp);
+    }
+};
