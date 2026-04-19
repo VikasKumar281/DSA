3123b6ddb85c95c678ac25c17b5bc23af0c29bdc
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-19 11:03:00 +0530

    Refactor smallestEqual function for clarity
    Refactor smallestEqual function to find the smallest index where the value equals the index modulo 10.

--- Arrays/Smallest Index With Equal Value.cpp
+++ Arrays/Smallest Index With Equal Value.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
         int n = nums.size();
+
+        for(int i =0;i<n;i++){
+            if(nums[i] == i % 10){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int n = nums.size();
         
         int mini = INT_MAX;
         for(int i =0;i<n;i++){
