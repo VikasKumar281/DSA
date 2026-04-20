dce675036b661a45a4681dc52f2e57a42034f0c1
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-20 19:33:21 +0530

    Update Smallest Stable Index I.cpp

--- Arrays/Smallest Stable Index I.cpp
+++ Arrays/Smallest Stable Index I.cpp
@@ -3,6 +3,36 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
 
+        vector<int> prefixSum(n), suffixSum(n);
+
+        prefixSum[0] = nums[0];
+        for(int i =1;i<n;i++){
+            prefixSum[i] = max(prefixSum[i-1], nums[i]);
+        }
+
+        suffixSum[n-1] = nums[n-1];
+        for(int i =n-2;i>=0;i--){
+            suffixSum[i] = min(suffixSum[i+1], nums[i]);
+        }        
+
+        for(int i = 0;i<n;i++){
+            if(prefixSum[i] - sufixSum[i] <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
         for(int i = 0;i<n;i++){
             int maxi = INT_MIN;
             int mini = INT_MAX;
