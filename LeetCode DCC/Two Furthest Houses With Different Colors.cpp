bcf92d4320cc7b0d43f27f28743cd9a74f0b1303
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-20 18:54:02 +0530

    Update Two Furthest Houses With Different Colors.cpp

--- LeetCode DCC/Two Furthest Houses With Different Colors.cpp
+++ LeetCode DCC/Two Furthest Houses With Different Colors.cpp
@@ -5,6 +5,30 @@ public:
 
         int maxi = INT_MIN;
         for(int i =0;i<n;i++){
+            if(colors[i] != colors[0]){
+                maxi = max(maxi,i);
+            }
+            if(colors[i] != colors[n-1]){
+                maxi = max(maxi,n-1-i);
+            }
+        }         
+       
+       return maxi;
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+
+        int maxi = INT_MIN;
+        for(int i =0;i<n;i++){
             for(int j =0;j<n;j++){
                 if(colors[i] != colors[j]){
                     maxi = max(maxi,abs(j-i));
