901ef31a842aecf2b48381f240e2dcaa51f5ba8b
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-20 19:25:50 +0530

    Create Smallest Stable Index I.cpp

---
+++ Arrays/Smallest Stable Index I.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        for(int i = 0;i<n;i++){
+            int maxi = INT_MIN;
+            int mini = INT_MAX;
+            
+            for(int j =0;j<=i;j++){
+                if(nums[j] > maxi){
+                    maxi = nums[j];
+                }
+            }
+            
+            for(int k =i;k<n;k++){
+                if(nums[k] < mini){
+                    mini = nums[k];
+                }
+            }
+
+            if(maxi - mini <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
