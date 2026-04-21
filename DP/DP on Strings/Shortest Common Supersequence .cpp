a6028050ec221d9a5cbda21ab1db99955a2ce029
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-22 00:27:19 +0530

    Update Shortest Common Supersequence .cpp

--- DP/DP on Strings/Shortest Common Supersequence .cpp
+++ DP/DP on Strings/Shortest Common Supersequence .cpp
@@ -1,3 +1,33 @@
+// Approach => Bottom Up ---------------------------------------------------------------------->
+// Time: O(m * n)
+// Space: O(m * n)
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2){
+        int m= s1.size();
+        int n = s2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        
+        for(int i =0;i<m+1; i++){
+            for(int j = 0;j<n+1;j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = i+j;
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
+
+
+
 // Approach => Memoization + Recursion ---------------------------------------------------------------------->
 // Time: O(n1 * n2)
 // Space: O(n1 * n2)
