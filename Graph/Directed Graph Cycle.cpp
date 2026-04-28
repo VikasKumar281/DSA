b13348af7a1f5b6a33b4d83d0214739eae2d6b1f
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-28 20:05:28 +0530

    Create Directed Graph Cycle.cpp

---
+++ Graph/Directed Graph Cycle.cpp
@@ -0,0 +1,42 @@
+// Approach => Using DFS
+class Solution {
+  public:
+    bool isCycleDFS(vector<vector<int>> &adj, int u, vector<bool> &visited, vector<bool> &inRecursion){
+        visited[u] = true;
+        inRecursion[u] = true;
+        
+        for(int &v: adj[u]){
+            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion)){
+                return true;
+            }
+            else if(inRecursion[v] == true){
+                return true;
+            }
+        }
+        
+        
+        inRecursion[u] = false;
+        return false;
+    } 
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<bool> visited(V, false);
+        vector<bool> inRecursion(V, false);
+        
+        for(int i =0;i<V;i++){
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
