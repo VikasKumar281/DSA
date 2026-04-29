249a43c17ba4b74d6c0aae387dc249020a69cc36
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-29 13:17:51 +0530

    Create  Topological Sort.cpp

---
+++ Graph/Topological Sort.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    void DFS(vector<vector<int>> &adj, int u , vector<bool> &visited, stack<int> &st){
+        if(visited[u] == true){
+            return;
+        }
+        
+        visited[u] = true;
+        
+        for(int &v: adj[u]){
+            DFS(adj, v, visited, st);
+        }
+        
+        st.push(u);
+    }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+      vector<vector<int>> adj(V);
+      
+      for(auto &e : edges){
+          int u = e[0];
+          int v = e[1];
+          
+          adj[u].push_back(v);
+      }
+      
+      vector<bool> visited(V, false);
+      stack<int> st;
+      
+      for(int i =0;i<V;i++){
+          if(!visited[i]){
+              DFS(adj, i, visited, st);
+          }
+      }
+      
+      vector<int> result;
+      while(!st.empty()){
+          result.push_back(st.top());
+          st.pop();
+      }
+      
+      return result;
+    }
+};
