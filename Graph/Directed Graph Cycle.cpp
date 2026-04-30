b70a1b24f5aaf56fc4bb450b8c43b73f9c62114b
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-30 19:23:50 +0530

    Update Directed Graph Cycle.cpp

--- Graph/Directed Graph Cycle.cpp
+++ Graph/Directed Graph Cycle.cpp
@@ -1,4 +1,64 @@
+// Approach => Using BFS (Kahn's Algorithm is used) ------------------------------------------------------------------------>
+// Time Complexity : O(V + E)
+// Space Complexity : O(V + E)
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<int> inDegree(V, 0);
+        
+        for(int u =0;u<V;u++){
+            for(auto &v : adj[u]){
+                inDegree[v]++;
+            }
+        }
+        
+        queue<int> que;
+        for(int i = 0;i<V;i++){
+            if(inDegree[i] == 0){
+                que.push(i);
+            }
+        }
+        
+        int count = 0;
+        while(!que.empty()){
+            count++;
+            int u = que.front();
+            que.pop();
+            
+            for(int &v: adj[u]){
+                inDegree[v]--;
+                
+                if(inDegree[v] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        
+        if(count == V){
+            return false;
+        }
+        
+        return true;
+    }
+};
+
+
+
+
+
+
 // Approach => Using DFS
+// Time Complexity : O(V + E)
+// Space Complexity : O(V + E)
 class Solution {
   public:
     bool isCycleDFS(vector<vector<int>> &adj, int u, vector<bool> &visited, vector<bool> &inRecursion){
