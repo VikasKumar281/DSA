03ca77400ff773a6fb66fac2f6a18dbabdc498c9
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-01 13:30:30 +0530

    Update Number of Provinces.cpp

--- Graph/Problems on BFS,DFS/Number of Provinces.cpp
+++ Graph/Problems on BFS,DFS/Number of Provinces.cpp
@@ -1,3 +1,55 @@
+//Approach => Using BFS ------------------------------------------------------------------------------------>
+class Solution {
+public:
+    void BFS(vector<vector<int>> &adj, int u, vector<bool> &visited){
+        queue<int> que;
+
+        que.push(u);
+        visited[u] = true;
+
+        while(!que.empty()){
+            int curr = que.front();
+            que.pop();
+
+            for(int &v : adj[curr]){
+                if(!visited[v]){
+                    BFS(adj, v, visited);
+                }
+            }
+        }
+
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adj(V);
+        
+        for(int i=0;i<V;i++){
+            for(int j =0;j<V;j++){
+                if(isConnected[i][j] == 1 && i != j){
+                  adj[i].push_back(j);
+                  adj[j].push_back(i);
+                }
+            }
+        }
+        
+        int count = 0;
+        vector<bool> visited(V, false);
+        for(int i = 0;i < V;i++){
+            if(!visited[i]){
+                BFS(adj, i, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
+
+
+//Approach => Using BFS ------------------------------------------------------------------------------------>
 class Solution {
 public:
     void DFS(vector<vector<int>> &adj, int u, vector<bool> &visited){
@@ -18,6 +70,7 @@ public:
             for(int j =0;j<V;j++){
                 if(isConnected[i][j] == 1 && i != j){
                   adj[i].push_back(j);
+                  adj[j].push_back(i);
                 }
             }
         }
