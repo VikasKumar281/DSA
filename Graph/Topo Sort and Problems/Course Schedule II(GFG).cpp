e35ce9640b696ccbc465aed179caffee0a2a387f
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-01 18:24:21 +0530

    Update Course Schedule II(GFG).cpp

--- Graph/Topo Sort and Problems/Course Schedule II(GFG).cpp
+++ Graph/Topo Sort and Problems/Course Schedule II(GFG).cpp
@@ -1,4 +1,64 @@
-using DFS ------------------------------------------------------------------------------------------------------------------------------->
+// Using BFS ------------------------------------------------------------------------------------------------------------------------------->
+class Solution {
+  public:
+    vector<int> topologicalSort(vector<vector<int>> &adj, int n, vector<int> &inDegree){
+        queue<int> que;
+        vector<int> result;
+        int count = 0;
+        
+        for(int i =0;i<n;i++){
+            if(inDegree[i] == 0){
+                result.push_back(i);
+                count++;
+                que.push(i);
+            }
+        }
+        
+        while(!que.empty()){
+            int curr = que.front();
+            que.pop();
+            
+            for(int &v : adj[curr]){
+                inDegree[v]--;
+                
+                if(inDegree[v] == 0){
+                    result.push_back(v);
+                    count++;
+                    que.push(v);
+                }
+            }
+        }
+        
+        if(count == n){
+            // No Cycle is present.
+            return result;
+        }
+        
+        return {};
+    }
+    vector<int> findOrder(int n, vector<vector<int>> &prerequisites) {
+      vector<vector<int>> adj(n);
+      vector<int> inDegree(n, 0);
+      
+      for(auto &p : prerequisites){
+          int u = p[1];
+          int v = p[0];
+          
+          // u -------> v
+          adj[u].push_back(v);
+          
+          inDegree[v]++;
+      }
+      
+      return topologicalSort(adj, n, inDegree);
+    }
+};
+
+
+
+
+
+// Using DFS ------------------------------------------------------------------------------------------------------------------------------->
 class Solution {
   public:
     bool isCycleDFS(vector<vector<int>> &adj, int u, vector<bool> &visited, vector<bool> &inRecursion, stack<int> &st){
