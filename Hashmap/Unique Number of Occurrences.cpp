b3690dc53f55566716743057e8b8ca4b876f429c
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-01 17:51:49 +0530

    Create Unique Number of Occurrences.cpp

---
+++ Hashmap/Unique Number of Occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        for(auto &i: arr){
+            mp[i]++;
+        }
+
+        unordered_set<int> st;
+        for(auto &i : mp){
+            if(st.count(i.second)){
+                return false;
+            }
+
+            st.insert(i.second);
+        }
+
+        return true;
+    }
+};
