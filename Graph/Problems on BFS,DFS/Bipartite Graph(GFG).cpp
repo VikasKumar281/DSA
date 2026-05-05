b7daa2c09f8e2b2e463a718a0ed0e6f78221b77a
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-05 20:21:05 +0530

    Create Bipartite Graph(GFG).cpp

---
+++ Graph/Problems on BFS,DFS/Bipartite Graph(GFG).cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    bool checkBipartiteDFS(vector<vector<int>> &adj, int curr, vector<int> &color, int currColor){
+        color[curr] = currColor;
+
+        for(int &v : adj[curr]){
+            int colorV;
+            if(color[v] == color[curr]){
+                return false;
+            }
+            if(color[v] == -1){
+                colorV = 1 - currColor;
+                if(checkBipartiteDFS(adj, v, color, colorV) == false){
+                  return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        for(auto &e: edges){
+            int u = e[0];
+            int v = e[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int> color(V, -1);
+      
+        // green color => 1;
+        // grey color => 0;
+      
+        for(int i =0;i<V;i++){
+            if(color[i] == -1){
+                if(checkBipartiteDFS(adj, i, color, 1) == false){
+                    return false;
+                }
+            }
+        }
+
+        return true;      
+    }
+};
