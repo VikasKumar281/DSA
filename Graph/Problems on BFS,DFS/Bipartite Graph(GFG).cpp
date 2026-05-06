fb96cb2cb53efceadd75646389e9c628f9ddeca9
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-06 20:52:51 +0530

    Update Bipartite Graph(GFG).cpp

--- Graph/Problems on BFS,DFS/Bipartite Graph(GFG).cpp
+++ Graph/Problems on BFS,DFS/Bipartite Graph(GFG).cpp
@@ -1,3 +1,63 @@
+// Using BFS ------------------------------------------------------------------------------------------------------>
+class Solution {
+  public:
+    bool checkBipartiteBFS(vector<vector<int>> &adj, int curr, vector<int> &color, int currColor){
+        queue<int> que;
+        que.push(curr);
+        color[curr] = currColor;
+
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            
+            for(int &v : adj[u]){
+              if(color[v] == color[u]){
+                return false;
+              }
+              if(color[v] == -1){
+                color[v] = 1 - color[u];
+                que.push(v);
+              }
+            }
+        }
+
+        return true;
+    }
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        for(auto &e: edges){
+            int u = e[0];
+            int v = e[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int> color(V, -1);
+      
+        // green color => 1;
+        // grey color => 0;
+      
+        for(int i =0;i<V;i++){
+            if(color[i] == -1){
+                if(checkBipartiteBFS(adj, i, color, 1) == false){
+                    return false;
+                }
+            }
+        }
+
+        return true;      
+    }
+};
+
+
+
+
+
+
+
+
+// Using DFS ------------------------------------------------------------------------------------------------------>
 class Solution {
   public:
     bool checkBipartiteDFS(vector<vector<int>> &adj, int curr, vector<int> &color, int currColor){
