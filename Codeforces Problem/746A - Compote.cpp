8cbe816b10475d83b778809c531a02d55bb8a9fa
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-19 20:10:10 +0530

    Create 746A - Compote.cpp

---
+++ Codeforces Problem/746A - Compote.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >>a>>b>>c;
+    
+    int x =min({a,b/2,c / 4});
+    
+    cout<<x*7<<endl;
+    
+    return 0;
+}
