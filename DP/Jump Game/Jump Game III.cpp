b19a43f1fa3523fb2af45acc6e22829261031f67
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-27 15:36:27 +0530

    Update Jump Game III.cpp

--- DP/Jump Game/Jump Game III.cpp
+++ DP/Jump Game/Jump Game III.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    bool solve(vector<int>& arr, int i, vector<int> &dp){
+        int n = arr.size();
+        if(i < 0 || i >= n || arr[i] < 0){
+            return false;
+        }
+        if(arr[i] == 0){
+            return true;
+        }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        arr[i] *= -1;
+        
+        bool a = solve(arr, i + arr[i], dp);
+        bool b = solve(arr, i - arr[i], dp);
+
+        return dp[i] = a || b;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector<int> dp(arr.size(), -1);
+        return solve(arr, start, dp);
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
     bool solve(vector<int>& arr, int i){
         int n = arr.size();
         if(i < 0 || i >= n || arr[i] < 0){
