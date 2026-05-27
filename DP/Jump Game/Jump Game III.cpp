96920dc1d79c5d889e67538932e41c95700efafb
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-27 15:34:24 +0530

    Create Jump Game III.cpp

---
+++ DP/Jump Game/Jump Game III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(vector<int>& arr, int i){
+        int n = arr.size();
+        if(i < 0 || i >= n || arr[i] < 0){
+            return false;
+        }
+        if(arr[i] == 0){
+            return true;
+        }
+
+        arr[i] *= -1;
+        
+        bool a = solve(arr, i + arr[i]);
+        bool b = solve(arr, i - arr[i]);
+
+        return a || b;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        return solve(arr, start);
+    }
+};
