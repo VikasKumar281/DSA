4ee2d5efefe23d7b3921e413f2b92c18493f433b
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-27 23:41:57 +0530

    Create 716A - Crazy Computer.cpp

---
+++ Codeforces Problem/716A - Crazy Computer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, c;
+    cin >> n >> c;
+
+    vector<int> t(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> t[i];
+    }
+
+    int ans = 1;
+
+    for (int i = 1; i < n; i++){
+        if (t[i] - t[i - 1] <= c){
+            ans++;
+        } else {
+            ans = 1;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
