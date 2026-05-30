1d4c3f123f9ece2fe0c38a4475ae8789c5dba1fc
Vikas Kumar <vikaskumar280204@gmail.com>
2026-05-30 23:36:38 +0530

    Create Array Operations.cpp

---
+++ Arrays/Easy Problems GFG/Array Operations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int arrayOperations(int n, vector<int> &arr) {
+        bool zeroPresent = false;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                zeroPresent = true;
+                break;
+            }
+        }
+        
+        if(!zeroPresent){
+            return -1;
+        }
+        
+        int operations = 0;
+        int i = 0;
+        
+        while(i < n){
+            if(arr[i]!= 0){
+                operations++;
+                
+                while(i < n && arr[i] != 0){
+                    i++;
+                }
+            } 
+            else{
+                i++;
+            }
+        }
+        
+        return operations;
+    }
+};
