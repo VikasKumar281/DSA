455f8022d9308e2d14c51eba5d0f73e828e23ed6
Vikas Kumar <vikaskumar280204@gmail.com>
2026-06-05 21:52:54 +0530

    Create 681A - A Good Contest.cpp

---
+++ Codeforces Problem/681A - A Good Contest.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    bool ok = false;
+
+    while(n--){
+        string name;
+        int before,after;
+        cin >> name>>before>>after;
+
+        if(before >= 2400 &&after > before){
+            ok = true;
+        }
+    }
+
+    cout<<(ok ? "YES" : "NO") << '\n';
+
+    return 0;
+}
