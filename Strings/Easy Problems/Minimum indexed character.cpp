bbfea5a4a8b9a0e9cd545c6cd8fea50456275dca
Vikas Kumar <vikaskumar280204@gmail.com>
2026-06-11 10:27:03 +0530

    Update Minimum indexed character.cpp

--- Strings/Easy Problems/Minimum indexed character.cpp
+++ Strings/Easy Problems/Minimum indexed character.cpp
@@ -1,5 +1,26 @@
 class Solution {
   public:
+    int minIndexChar(string &s1, string &s2) {
+        unordered_set<char> st;
+
+        for (char ch : s2) {
+            st.insert(ch);
+        }
+
+        for (int i = 0; i < s1.size(); i++) {
+            if (st.count(s1[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+class Solution {
+  public:
     string printMinIndexChar(string S, string patt) {
         int n1 = S.size();
         int n2 = patt.size();
