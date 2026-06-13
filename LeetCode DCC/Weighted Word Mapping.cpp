536fda3a6eef52dd619871eb8a853ad723adecc2
Vikas Kumar <vikaskumar280204@gmail.com>
2026-06-13 22:42:37 +0530

    Create Weighted Word Mapping.cpp

---
+++ LeetCode DCC/Weighted Word Mapping.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for(string &word : words){
+            int sum = 0;
+
+            for(char ch : word){
+                sum+=weights[ch-'a'];
+            }
+
+            int rem = sum %26;
+            ans +=char('z'- rem);
+        }
+
+        return ans;
+    }
+};
