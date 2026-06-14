f3281779c0341c88b690633e692902404ffacf75
Vikas Kumar <vikaskumar280204@gmail.com>
2026-06-14 19:49:06 +0530

    Create 599A - Patrick and Shopping.cpp

---
+++ Codeforces Problem/599A - Patrick and Shopping.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long d1, d2, d3;
+    cin >>d1>>d2>>d3;
+
+    cout <<min({
+        2 * d1 + 2 * d2,
+        d1 + d2 + d3,
+        2 * d1 + 2 * d3,
+        2 * d2 + 2 * d3
+    });
+
+    return 0;
+}
