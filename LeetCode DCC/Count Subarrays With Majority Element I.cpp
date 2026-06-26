dbfe3e806227ef1b57c655ce3baa9e5f95541187
Vikas Kumar <vikaskumar280204@gmail.com>
2026-06-26 19:52:16 +0530

    Update Count Subarrays With Majority Element I.cpp

--- LeetCode DCC/Count Subarrays With Majority Element I.cpp
+++ LeetCode DCC/Count Subarrays With Majority Element I.cpp
@@ -20,3 +20,32 @@ public:
         return result;
     }
 };
+
+
+// T.C. = O(n^3) 
+// S.C. = O(1)
+// Time Limit Exceeded -------------------------------------->
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int result = 0;
+        for(int i =0;i<n;i++){
+           for(int j = i;j<n;j++){
+            int count = 0;
+            for(int k = i;k<=j;k++){
+                if(nums[k] == target){
+                    count++;
+                }
+            }
+
+            if(count > (j-i+1)/2){
+                result++;
+            }
+           }
+        }
+
+        return result;
+    }
+};
