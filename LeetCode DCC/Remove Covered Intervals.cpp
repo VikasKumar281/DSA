316ad83799e32d11948587b3db3061eec35da273
Vikas Kumar <vikaskumar280204@gmail.com>
2026-07-08 12:02:28 +0530

    Update Remove Covered Intervals.cpp

--- LeetCode DCC/Remove Covered Intervals.cpp
+++ LeetCode DCC/Remove Covered Intervals.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        vector<vector<int>> result;
+
+        // CUSTOM COMPARISON --------------------------->
+        auto lambda = [](vector<int> &vec1 , vector<int> &vec2){
+            if(vec1[0] == vec2[0]){
+                return vec1[1] > vec2[1];
+            }
+
+            return vec1[0] < vec2[0];
+        }; 
+
+        sort(begin(intervals), end(intervals), lambda);
+
+        result.push_back(intervals[0]);
+        for(int i = 1;i<n;i++){
+            if(result.back()[0] <= intervals[i][0] && result.back()[1] >= intervals[i][1]){
+                continue;
+            }
+
+            result.push_back(intervals[i]);
+        }
+
+
+        return result.size(); 
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(),
              [](const vector<int>& a, const vector<int>& b) {
                  if (a[0] == b[0])
